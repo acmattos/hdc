951efbc19126809a4dcfd8d077a2c87b11c1ab7c
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-09-21 11:04:09 -0300

    Adding ULIDGen

---
+++ src/main/kotlin/br/com/acmattos/hdc/common/tool/uid/ULIDGen.kt
@@ -0,0 +1,45 @@
+package br.com.acmattos.hdc.common.tool.uid
+
+import br.com.acmattos.hdc.common.tool.logable.Loggable
+import de.huxhorn.sulky.ulid.ULID
+import java.security.SecureRandom
+
+/**
+ * @author ACMattos
+ * @since 12/06/2020.
+ */
+open class ULIDGen {
+    private val ulid: ULID = ULID(
+        SecureRandom(
+            (this.javaClass.name.substringBefore("\$Companion")
+                + System.currentTimeMillis()).toByteArray()
+        )
+    )
+
+    init {
+        logger.trace(
+            "ULIDGen created for: [{}]",
+            this.javaClass.name.substringBefore("\$Companion")
+        )
+    }
+
+    fun nextULID(): String = ulid.nextULID()
+
+    companion object: Loggable() {
+        fun parseULID(candidate: String):String {
+            try {
+                return ULID.parseULID(candidate).toString()
+            } catch(ex: IllegalArgumentException) {
+                if("ulidString must be exactly 26 chars long." === ex.message ) {
+                    throw IllegalArgumentException(
+                        "[$candidate] does not match the expected size: 26 chars long!"
+                    )
+                } else {
+                    throw IllegalArgumentException(
+                        "[$candidate] must not exceed '7ZZZZZZZZZZZZZZZZZZZZZZZZZ'!"
+                    )
+                }
+            }
+        }
+    }
+}
