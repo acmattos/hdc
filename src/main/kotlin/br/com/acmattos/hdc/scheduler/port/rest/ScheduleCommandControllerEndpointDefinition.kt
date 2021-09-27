aca645eb6614b0ab24074f0311cfac814a1ef6d9
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-09-27 07:50:33 -0300

    Adding ScheduleCommandControllerEndpointDefinition

---
+++ src/main/kotlin/br/com/acmattos/hdc/scheduler/port/rest/ScheduleCommandControllerEndpointDefinition.kt
@@ -0,0 +1,19 @@
+package br.com.acmattos.hdc.scheduler.port.rest
+
+import br.com.acmattos.hdc.common.context.port.rest.EndpointDefinition
+import io.javalin.apibuilder.ApiBuilder.get
+import io.javalin.apibuilder.ApiBuilder.path
+
+/**
+* @author ACMattos
+* @since 27/03/2021.
+*/
+class ScheduleCommandControllerEndpointDefinition(
+    private val controller: ScheduleCommandController
+): EndpointDefinition {
+    override fun routes() {
+        path("schedules") {
+            get(controller::createScheduleForADentist)
+        }
+    }
+}
---
+++ src/test/kotlin/br/com/acmattos/hdc/scheduler/port/rest/ScheduleCommandControllerEndpointDefinitionTest.kt
@@ -0,0 +1,39 @@
+package br.com.acmattos.hdc.scheduler.port.rest
+
+import br.com.acmattos.hdc.common.context.port.rest.EndpointDefinition
+import br.com.acmattos.hdc.scheduler.config.ENDPOINT_DEFINITION
+import br.com.acmattos.hdc.scheduler.config.ScheduleKoinComponent
+import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder
+import org.koin.core.Koin
+import org.koin.core.qualifier.named
+import org.koin.dsl.koinApplication
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.gherkin.Feature
+
+/**
+ * @author ACMattos
+ * @since 27/09/2021.
+ */
+object ScheduleCommandControllerEndpointDefinitionTest: Spek({
+    Feature("${ScheduleCommandControllerEndpointDefinition::class.java} existence") {
+        Scenario("endpoint definition exists") {
+            lateinit var koin: Koin
+            lateinit var endpoint: EndpointDefinition
+            Given("""a Koin Application properly instantiated""") {
+                koin = koinApplication {
+                    modules(ScheduleKoinComponent.loadModule())
+                }.koin
+            }
+            And("""a javalin server is configured""") {
+                ApiBuilder.setStaticJavalin(Javalin.create())
+            }
+            When("""a the endpoint definition is injected""") {
+                 endpoint = koin.get<EndpointDefinition>(named(ENDPOINT_DEFINITION))
+            }
+            Then("""#routes can be called""") {
+                endpoint.routes()
+            }
+        }
+    }
+})
