987244b0572ec68bce6695c6739315325e915282
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-10-13 19:15:26 -0300

    adding constant name for endpoint definition

--- src/main/kotlin/br/com/acmattos/hdc/scheduler/config/ScheduleKoinComponent.kt
+++ src/main/kotlin/br/com/acmattos/hdc/scheduler/config/ScheduleKoinComponent.kt
@@ -7,7 +7,8 @@ import org.koin.core.component.KoinComponent
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
-const val EP = "ScheduleCommandControllerEndpointDefinition"
+const val ED = "ScheduleCommandControllerEndpointDefinition"
+
 /**
  * @author ACMattos
  * @since 14/08/2019.
@@ -15,9 +16,7 @@ const val EP = "ScheduleCommandControllerEndpointDefinition"
 object ScheduleKoinComponent: KoinComponent {
     fun loadModule() = module {
         // 1 - Endpoint Definition
-        single<EndpointDefinition>(
-            named("ScheduleCommandControllerEndpointDefinition")
-        ) {
+        single<EndpointDefinition>(named(ED)) {
             ScheduleCommandControllerEndpointDefinition(get())
         }
         // 2 - Controller Endpoint
