20ffb80c9240167a7add3473252481f154ca2b61
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-10-13 17:53:18 -0300

    adding enums

---
+++ src/main/kotlin/br/com/acmattos/hdc/common/context/config/ContextLogEnum.kt
@@ -0,0 +1,9 @@
+package br.com.acmattos.hdc.common.context.config
+
+/**
+ * @author ACMattos
+ * @since 05/10/2021.
+ */
+enum class ContextLogEnum {
+    REPOSITORY, SERVICE,
+}
---
+++ src/main/kotlin/br/com/acmattos/hdc/common/context/port/rest/EndpointLogEnum.kt
@@ -0,0 +1,9 @@
+package br.com.acmattos.hdc.common.context.port.rest
+
+/**
+ * @author ACMattos
+ * @since 30/09/2021.
+ */
+enum class EndpointLogEnum {
+    ENDPOINT
+}
