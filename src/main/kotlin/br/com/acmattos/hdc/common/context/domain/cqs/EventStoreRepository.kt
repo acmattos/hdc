a4c0efe1c65b60b195a4e5e87618e88136f8b3a9
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-10-13 21:05:42 -0300

    adding EventStore

---
+++ src/main/kotlin/br/com/acmattos/hdc/common/context/domain/cqs/EventStoreRepository.kt
@@ -0,0 +1,32 @@
+package br.com.acmattos.hdc.common.context.domain.cqs
+
+import br.com.acmattos.hdc.common.context.domain.cqs.EventStoreEnum.STORE
+import br.com.acmattos.hdc.common.context.port.persistence.mongodb.MdbEventDocument
+import br.com.acmattos.hdc.common.context.port.persistence.mongodb.MdbRepository
+import br.com.acmattos.hdc.common.tool.loggable.Loggable
+
+/**
+ * @author ACMattos
+ * @since 01/10/2021.
+ */
+class EventStoreRepository<T: Event>(
+    private val mdbRepository: MdbRepository<MdbEventDocument>
+): EventStore<T> {
+   override fun addEvent(event: T) {
+        mdbRepository.save(MdbEventDocument(event))
+        logger.trace(
+            "[{}] Add event to store {}...: -> !DONE! <-",
+            STORE.name,
+            event.javaClass.name
+        )
+    }
+    companion object: Loggable()
+}
+
+/**
+ * @author ACMattos
+ * @since 12/10/2021.
+ */
+enum class EventStoreEnum {
+    STORE;
+}
