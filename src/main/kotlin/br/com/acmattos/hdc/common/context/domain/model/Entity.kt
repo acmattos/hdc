990c1f976521e383b1222faec3d627b924e6a57f
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-10-13 21:13:51 -0300

    adding Entity

---
+++ src/main/kotlin/br/com/acmattos/hdc/common/context/domain/model/Entity.kt
@@ -0,0 +1,13 @@
+package br.com.acmattos.hdc.common.context.domain.model
+
+/**
+ * @author acmattos
+ * @since 27/07/2019.
+ */
+interface Entity
+
+/**
+ * @author ACMattos
+ * @since 28/06/2020.
+ */
+interface ValueObject
