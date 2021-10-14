32fbb6711ee2b55000d59264370560dc8e67a8f8
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-10-13 21:18:38 -0300

    adding PersonMdbDocument

---
+++ src/main/kotlin/br/com/acmattos/hdc/person/port/persistence/mongodb/PersonMdbDocument.kt
@@ -0,0 +1,47 @@
+package br.com.acmattos.hdc.person.port.persistence.mongodb
+
+import br.com.acmattos.hdc.common.context.port.persistence.mongodb.MdbDocument
+import br.com.acmattos.hdc.person.domain.model.Person
+import br.com.acmattos.hdc.person.domain.model.PersonId
+import br.com.acmattos.hdc.person.domain.model.PersonType
+import java.time.LocalDateTime
+
+/**
+ * @author ACMattos
+ * @since 04/10/2021.
+ */
+class PersonMdbDocument(
+    val personId: String,
+    val fullName: String,
+    val personType: String,
+    val addresses: List<AddressMdbDocument>,
+    val contacts: List<ContactMdbDocument>,
+    val enabled: Boolean,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime?
+): MdbDocument() {
+    constructor(
+        person: Person
+    ): this(
+        personId = person.personId.id,
+        fullName = person.fullName,
+        personType = person.personType.name,
+        addresses = person.addresses.map { AddressMdbDocument(it) },
+        contacts = person.contacts.map { ContactMdbDocument(it) },
+        enabled = person.enabled,
+        createdAt = person.createdAt,
+        updatedAt = person.updatedAt,
+    )
+
+    override fun toType(): Person =
+        Person(
+            personIdData = PersonId(personId),
+            fullNameData = fullName,
+            personTypeData = PersonType.convert(personType),
+            addressesData = addresses.map { it.toType() },
+            contactsData = contacts.map { it.toType() },
+            enabledData = enabled,
+            createdAtData = createdAt,
+            updatedAtData = updatedAt
+        )
+}
