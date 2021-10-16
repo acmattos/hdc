4f634e2d57855a44a977089782674696ca2e9351
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-10-15 21:48:15 -0300

    adding Repository

---
+++ src/main/kotlin/br/com/acmattos/hdc/common/context/domain/model/Repository.kt
@@ -0,0 +1,10 @@
+package br.com.acmattos.hdc.common.context.domain.model
+
+/**
+ * @author ACMattos
+ * @since 25/07/2019.
+ */
+interface Repository<T> {
+    fun save(entity: T)
+    fun findByField(fieldName: String, value: Any): T?
+}
