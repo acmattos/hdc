fccbf99ebf3e7653fd959ca68c864e8dd347ff3e
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-10-21 19:09:45 -0300

    adding header

--- src/test/kotlin/br/com/acmattos/hdc/person/port/rest/PersonRequestBuilder.kt
+++ src/test/kotlin/br/com/acmattos/hdc/person/port/rest/PersonRequestBuilder.kt
@@ -1,5 +1,9 @@
 package br.com.acmattos.hdc.person.port.rest
 
+/**
+ * @author ACMattos
+ * @since 16/10/2021.
+ */
 object PersonRequestBuilder {
     fun buildCreateADentistRequest() = CreateADentistRequest(
         "fullName",
