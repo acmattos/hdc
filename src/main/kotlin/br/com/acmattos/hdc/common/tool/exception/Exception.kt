bd02f3e0143d8a41deeb88fa4c6f8ff5c8e6eb05
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-10-30 14:52:02 -0300

    adding ErrorTrackerCode

--- src/main/kotlin/br/com/acmattos/hdc/common/tool/exception/Exception.kt
+++ src/main/kotlin/br/com/acmattos/hdc/common/tool/exception/Exception.kt
@@ -1,13 +1,23 @@
 package br.com.acmattos.hdc.common.tool.exception
 
+import br.com.acmattos.hdc.common.exception.HdcGenericException
+import br.com.acmattos.hdc.common.tool.server.javalin.ErrorTrackerCode
+
 /**
  * @author ACMattos
  * @since 30/06/2020.
  */
-class InternalServerErrorException(message: String, th: Throwable): Exception(message, th)
+class InternalServerErrorException(
+    message: String,
+    val code: ErrorTrackerCode,
+    throwable: Throwable
+): HdcGenericException(message, code, throwable)
 
-/**
- * @author ACMattos
- * @since 16/08/2021.
- */
-class NotFoundException(message: String): Exception(message)
+///**
+// * @author ACMattos
+// * @since 16/08/2021.
+// */
+//class NotFoundException(
+//    message: String,
+//    code: String
+//): HdcGenericException(message, code)
