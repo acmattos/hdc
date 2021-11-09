b023ce75af6500e2538771834200fbb4d65511c6
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-11-09 10:50:09 -0300

    adding dentist data

--- src/main/kotlin/br/com/acmattos/hdc/scheduler/domain/cqs/ScheduleEvent.kt
+++ src/main/kotlin/br/com/acmattos/hdc/scheduler/domain/cqs/ScheduleEvent.kt
@@ -2,8 +2,8 @@ package br.com.acmattos.hdc.scheduler.domain.cqs
 
 import br.com.acmattos.hdc.common.context.domain.cqs.Event
 import br.com.acmattos.hdc.common.context.domain.model.AuditLog
+import br.com.acmattos.hdc.scheduler.domain.model.Dentist
 import br.com.acmattos.hdc.scheduler.domain.model.Period
-import br.com.acmattos.hdc.scheduler.domain.model.ScheduleDentistId
 import br.com.acmattos.hdc.scheduler.domain.model.ScheduleId
 import com.fasterxml.jackson.annotation.JsonTypeName
 import java.time.LocalDateTime
@@ -24,7 +24,7 @@ open class ScheduleEvent(
 @JsonTypeName("CreateAScheduleForTheDentistEvent")
 data class CreateAScheduleForTheDentistEvent(
     override val scheduleId: ScheduleId,
-    val scheduleDentistId: ScheduleDentistId,
+    val dentist: Dentist,
     val periods: List<Period>,
     val enabled: Boolean,
     val createdAt: LocalDateTime,
@@ -34,77 +34,10 @@ data class CreateAScheduleForTheDentistEvent(
         command: CreateAScheduleForTheDentistCommand
     ): this(
         scheduleId = command.scheduleId,
-        scheduleDentistId = command.scheduleDentistId,
+        dentist = command.dentist!!,
         periods = command.periods,
         enabled = command.enabled,
         createdAt = command.createdAt,
         auditLog = command.auditLog
     )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-///**
-// * @author ACMattos
-// * @since 10/07/2020.
-// */
-//@JsonTypeName("ModifySchedulePeriodsEvent")
-//data class ModifySchedulePeriodsEvent(
-//    override val tenantId: TenantId,
-//    override val dentistId: DentistId,
-//    override val scheduleId: ScheduleId,
-//    @JsonIgnore override val events: MutableList<ScheduleEvent> = mutableListOf<ScheduleEvent>(),
-//    val periods: List<Period>,
-//    val updatedAt: LocalDateTime,
-//    override val auditEntry: AuditEntry
-//): ScheduleEvent(tenantId, dentistId, scheduleId, events, auditEntry) {
-//    constructor(
-//        command: ModifySchedulePeriodsCommand
-//    ): this(
-//        tenantId = command.tenantId,
-//        dentistId = command.dentistId,
-//        scheduleId = command.scheduleId,
-//        periods = command.periods,
-//        updatedAt = command.updatedAt,
-//        auditEntry = command.auditEntry
-//    )
-//}
-//
-///**
-// * @author ACMattos
-// * @since 01/11/2020.
-// */
-//@JsonTypeName("ModifyScheduleEnabledStatusEvent")
-//data class ModifyScheduleEnabledStatusEvent(
-//    override val tenantId: TenantId,
-//    override val dentistId: DentistId,
-//    override val scheduleId: ScheduleId,
-//    @JsonIgnore override val events: MutableList<ScheduleEvent> = mutableListOf<ScheduleEvent>(),
-//    val enabled: Boolean,
-//    val updatedAt: LocalDateTime,
-//    override val auditEntry: AuditEntry
-//): ScheduleEvent(tenantId, dentistId, scheduleId, events, auditEntry) {
-//    constructor(
-//        command: ModifyScheduleEnabledStatusCommand
-//    ): this(
-//        tenantId = command.tenantId,
-//        dentistId = command.dentistId,
-//        scheduleId = command.scheduleId,
-//        enabled = command.enabled,
-//        updatedAt = command.updatedAt,
-//        auditEntry = command.auditEntry
-//    )
-//}
