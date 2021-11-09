5c52709d3eb39ca276f2732f13a94f8e651efb71
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-11-09 11:06:27 -0300

    adding optional return

--- src/main/kotlin/br/com/acmattos/hdc/common/context/domain/model/Repository.kt
+++ src/main/kotlin/br/com/acmattos/hdc/common/context/domain/model/Repository.kt
@@ -1,11 +1,13 @@
 package br.com.acmattos.hdc.common.context.domain.model
 
+import java.util.Optional
+
 /**
  * @author ACMattos
  * @since 25/07/2019.
  */
 interface Repository<T> {
     fun save(entity: T)
-    fun findByField(fieldName: String, value: Any): T?
-    fun findAllByField(fieldName: String, value: Any): List<T>?
+    fun findByField(fieldName: String, value: Any): Optional<T>
+    fun findAllByField(fieldName: String, value: Any): List<T>
 }
