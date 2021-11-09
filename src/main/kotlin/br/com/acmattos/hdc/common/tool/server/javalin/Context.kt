f65eed54e7d70fde03952132abf360e8f3565f7b
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-11-09 09:26:54 -0300

    adding query request getter

---
+++ src/main/kotlin/br/com/acmattos/hdc/common/tool/server/javalin/Context.kt
@@ -0,0 +1,14 @@
+package br.com.acmattos.hdc.common.tool.server.javalin
+
+import br.com.acmattos.hdc.common.context.port.rest.Request
+import io.javalin.http.Context
+
+/**
+ * @author ACMattos
+ * @since 08/11/2021.
+ */
+inline fun <reified T: Request<*>> Context.getQueryRequest(
+    createNew: (Context) -> T
+): T {
+    return createNew(this)
+}
