e7a45707f1103e17b1f0ff03ad4659b2c9e61772
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-11-11 17:55:33 -0300

    changing name to handle both queries and builders

--- src/main/kotlin/br/com/acmattos/hdc/common/tool/server/javalin/Context.kt
+++ src/main/kotlin/br/com/acmattos/hdc/common/tool/server/javalin/Context.kt
@@ -7,7 +7,7 @@ import io.javalin.http.Context
  * @author ACMattos
  * @since 08/11/2021.
  */
-inline fun <reified T: Request<*>> Context.getQueryRequest(
+inline fun <reified T: Request<*>> Context.getRequest(
     createNew: (Context) -> T
 ): T {
     return createNew(this)
