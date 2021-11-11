8688c843c0c6a0d9051da16139d35a0cef31e678
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-11-11 19:03:38 -0300

    adding endpoints routes logs

--- src/main/kotlin/br/com/acmattos/hdc/person/port/rest/PersonControllerEndpointDefinition.kt
+++ src/main/kotlin/br/com/acmattos/hdc/person/port/rest/PersonControllerEndpointDefinition.kt
@@ -1,10 +1,13 @@
 package br.com.acmattos.hdc.person.port.rest
 
 import br.com.acmattos.hdc.common.context.port.rest.EndpointDefinition
+import br.com.acmattos.hdc.common.tool.loggable.Loggable
 import io.javalin.apibuilder.ApiBuilder.get
 import io.javalin.apibuilder.ApiBuilder.path
 import io.javalin.apibuilder.ApiBuilder.post
 
+private const val PERSONS = "persons"
+private const val DENTIST_ID = ":dentist_id"
 /**
  * @author ACMattos
  * @since 05/10/2021.
@@ -14,11 +17,16 @@ class PersonControllerEndpointDefinition(
     private val query: PersonQueryController
 ): EndpointDefinition {
     override fun routes() {
-        path("persons") {
+        path(PERSONS) {
             post(command::createADentist)
-            path(":dentist_id") {
+            path(DENTIST_ID) {
                 get(query::findTheDentist)
             }
-       }
+        }
+        logger.info("Route loaded: -> POST /$PERSONS <-")
+        logger.info("Route loaded: -> GET /$PERSONS/$DENTIST_ID <-")
+        logger.info("All routes loaded for: -> ${PersonControllerEndpointDefinition::class.java} <-")
     }
+
+    companion object: Loggable()
 }
--- src/main/kotlin/br/com/acmattos/hdc/scheduler/port/rest/ScheduleControllerEndpointDefinition.kt
+++ src/main/kotlin/br/com/acmattos/hdc/scheduler/port/rest/ScheduleControllerEndpointDefinition.kt
@@ -1,8 +1,11 @@
 package br.com.acmattos.hdc.scheduler.port.rest
 
 import br.com.acmattos.hdc.common.context.port.rest.EndpointDefinition
+import br.com.acmattos.hdc.common.tool.loggable.Loggable
 import io.javalin.apibuilder.ApiBuilder.path
 import io.javalin.apibuilder.ApiBuilder.post
+
+private const val SCHEDULES = "schedules"
 
 /**
 * @author ACMattos
@@ -12,8 +15,12 @@ class ScheduleControllerEndpointDefinition(
     private val controller: ScheduleCommandController
 ): EndpointDefinition {
     override fun routes() {
-        path("schedules") {
+        path(SCHEDULES) {
             post(controller::createAScheduleForTheDentist)
         }
+        logger.info("Route loaded: -> POST /$SCHEDULES <-")
+        logger.info("All routes loaded for: -> ${ScheduleControllerEndpointDefinition::class.java} <-")
     }
+
+    companion object: Loggable()
 }
