abdc6c530816fa91ce51ad0f926e9328bd09f3d0
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-11-29 15:13:15 -0300

    adding Appointment

---
+++ src/main/kotlin/br/com/acmattos/hdc/scheduler/domain/model/Appointment.kt
@@ -0,0 +1,118 @@
+package br.com.acmattos.hdc.scheduler.domain.model
+
+import br.com.acmattos.hdc.common.context.domain.model.Entity
+import br.com.acmattos.hdc.common.context.domain.model.Id
+import br.com.acmattos.hdc.common.tool.assertion.Assertion
+import br.com.acmattos.hdc.common.tool.enum.assertThatTerm
+import br.com.acmattos.hdc.scheduler.config.ErrorTrackerCodeEnum.APPOINTMENT_STATUS_CONVERT_FAILED
+import br.com.acmattos.hdc.scheduler.config.ErrorTrackerCodeEnum.ENTITY_APPOINTMENT_STATUS_MUST_BE_FREED
+import br.com.acmattos.hdc.scheduler.config.ErrorTrackerCodeEnum.ENTITY_APPOINTMENT_ENABLED_MUST_BE_TRUE
+import br.com.acmattos.hdc.scheduler.config.ScheduleLogEnum.APPOINTMENT
+import br.com.acmattos.hdc.scheduler.domain.cqs.AppointmentEvent
+import br.com.acmattos.hdc.scheduler.domain.cqs.CreateAppointmentForTheScheduleEvent
+import br.com.acmattos.hdc.scheduler.domain.model.AppointmentStatus.FREED
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+
+/**
+ * @author ACMattos
+ * @since 09/07/2019.
+ */
+data class Appointment(
+    private var appointmentIdData: AppointmentId? = null,
+    private var scheduleIdData: ScheduleId? = null,
+    private var patientData: Patient? = null,
+    private var dateData: LocalDate? = null,
+    private var timeData: LocalTime? = null,
+    private var durationData: Long? = null,
+    private var statusData: AppointmentStatus = FREED,
+    private var enabledData: Boolean = true,
+    private var createdAtData: LocalDateTime = LocalDateTime.now(),
+    private var updatedAtData: LocalDateTime? = null
+): Entity {
+    val appointmentId get() = appointmentIdData!!
+    val scheduleId get() = scheduleIdData!!
+    val patient get() = patientData
+    val date get() = dateData!!
+    val time get() = timeData!!
+    val duration get() = durationData!!
+    val status get() = statusData
+    val enabled get() = enabledData
+    val createdAt get() = createdAtData
+    val updatedAt get() = updatedAtData
+
+    fun apply(events: List<AppointmentEvent>): Appointment {
+        for (event in events) {
+            apply(event)
+        }
+        return this
+    }
+
+    fun apply(event: AppointmentEvent): Appointment {
+        when(event) {
+            is CreateAppointmentForTheScheduleEvent -> apply(event)
+            else -> apply(event as CreateAppointmentForTheScheduleEvent)
+        }
+        return this
+    }
+
+    private fun apply(event: CreateAppointmentForTheScheduleEvent) {
+        appointmentIdData = event.appointmentId
+        scheduleIdData = event.scheduleId
+        dateData = event.date
+        timeData = event.time
+        durationData = event.duration
+        statusData = event.status
+        enabledData = event.enabled
+        createdAtData = event.createdAt
+        assertCreateAppointmentForTheScheduleEvent()
+    }
+
+    private fun assertCreateAppointmentForTheScheduleEvent() {
+        Assertion.assert(
+            "Status must be $FREED!",
+            APPOINTMENT.name,
+            ENTITY_APPOINTMENT_STATUS_MUST_BE_FREED.code
+        ) {
+            FREED == statusData
+        }
+        Assertion.assert(
+            "Enabled must be TRUE!",
+            APPOINTMENT.name,
+            ENTITY_APPOINTMENT_ENABLED_MUST_BE_TRUE.code
+        ) {
+            enabledData
+        }
+    }
+
+    companion object {
+        fun apply(event: AppointmentEvent) = Appointment().apply(event)
+        fun apply(events: List<AppointmentEvent>) = Appointment().apply(events)
+    }
+}
+
+/**
+ * @author ACMattos
+ * @since 17/09/2020.
+ */
+class AppointmentId: Id {
+    constructor(id: String): super(id)
+    constructor(): super()
+}
+
+/**
+ * @author ACMattos
+ * @since 10/11/2021.
+ */
+enum class AppointmentStatus {
+    FREED, SCHEDULED, CONFIRMED, RELEASED, MISSED;
+    companion object {
+        fun convert(term: String): WeekDay = assertThatTerm(
+            term,
+            "[$term] does not correspond to a valid AppointmentStatus!",
+            APPOINTMENT.name,
+            APPOINTMENT_STATUS_CONVERT_FAILED.code
+        )
+    }
+}
---
+++ src/test/kotlin/br/com/acmattos/hdc/scheduler/domain/model/AppointmentTest.kt
@@ -0,0 +1,133 @@
+package br.com.acmattos.hdc.scheduler.domain.model
+
+import br.com.acmattos.hdc.common.context.domain.model.AuditLog
+import br.com.acmattos.hdc.common.tool.assertion.AssertionFailedException
+import br.com.acmattos.hdc.scheduler.config.ErrorTrackerCodeEnum.ENTITY_APPOINTMENT_STATUS_MUST_BE_FREED
+import br.com.acmattos.hdc.scheduler.config.ErrorTrackerCodeEnum.ENTITY_APPOINTMENT_ENABLED_MUST_BE_TRUE
+import br.com.acmattos.hdc.scheduler.domain.cqs.CreateAppointmentForTheScheduleEvent
+import br.com.acmattos.hdc.scheduler.domain.model.AppointmentStatus.FREED
+import br.com.acmattos.hdc.scheduler.domain.model.AppointmentStatus.SCHEDULED
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import org.assertj.core.api.AbstractThrowableAssert
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThatCode
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.gherkin.Feature
+
+private const val MESSAGE_1 = "Status must be FREED!"
+private const val MESSAGE_2 = "Enabled must be TRUE!"
+
+/**
+ * @author ACMattos
+ * @since 29/11/2021.
+ */
+object AppointmentTest: Spek({
+    Feature("${Appointment::class.java} usage") {
+        Scenario("a valid appointment creation") {
+            lateinit var event: CreateAppointmentForTheScheduleEvent
+            lateinit var entity: Appointment
+            lateinit var assertion: AbstractThrowableAssert<*, out Throwable>
+            Given("a valid ${CreateAppointmentForTheScheduleEvent::class.java} instantiation is done") {
+                event = CreateAppointmentForTheScheduleEvent(
+                    AppointmentId("01FK96GENJKTN1BYZW6BRHFZFJ"),
+                    ScheduleId("01FK96GENJKTN1BYZW6BRHFZFJ"),
+                    "HDC",
+                    LocalDate.of(2021, 11, 29),
+                    LocalTime.of(14, 0,0),
+                    30,
+                    FREED,
+                    true,
+                    LocalDateTime.now(),
+                    AuditLog("who", "what")
+                )
+            }
+            When("a successful appointment instantiation is done") {
+                assertion = assertThatCode {
+                    entity = Appointment.apply(event)
+                }
+            }
+            Then("no exception is raised") {
+                assertion.doesNotThrowAnyException()
+            }
+            And("entity is not null") {
+                Assertions.assertThat(entity).isNotNull()
+            }
+        }
+
+        Scenario("an invalid appointment creation - invalid status") {
+            lateinit var event: CreateAppointmentForTheScheduleEvent
+            var entity: Appointment? = null
+            lateinit var assertion: AbstractThrowableAssert<*, out Throwable>
+            Given("an invalid ${CreateAppointmentForTheScheduleEvent::class.java} instantiation is done") {
+                event = CreateAppointmentForTheScheduleEvent(
+                    AppointmentId("01FK96GENJKTN1BYZW6BRHFZFJ"),
+                    ScheduleId("01FK96GENJKTN1BYZW6BRHFZFJ"),
+                    "HDC",
+                    LocalDate.of(2021, 11, 29),
+                    LocalTime.of(14, 0,0),
+                    30,
+                    SCHEDULED,
+                    true,
+                    LocalDateTime.now(),
+                    AuditLog("who", "what")
+                )
+            }
+            When("a unsuccessful appointment instantiation is done") {
+                assertion = assertThatCode {
+                    entity = Appointment.apply(event)
+                }
+            }
+            Then("""instantiation throws exception""") {
+                assertion.hasSameClassAs(AssertionFailedException(MESSAGE_1, ENTITY_APPOINTMENT_STATUS_MUST_BE_FREED.code))
+            }
+            And("""exception has code ${ENTITY_APPOINTMENT_STATUS_MUST_BE_FREED.code}""") {
+                assertion.hasFieldOrPropertyWithValue("code", ENTITY_APPOINTMENT_STATUS_MUST_BE_FREED.code)
+            }
+            And("""exception has message $MESSAGE_1""") {
+                assertion.hasMessage(MESSAGE_1)
+            }
+            And("""no entity was instantiated""") {
+                Assertions.assertThat(entity).isNull()
+            }
+        }
+
+        Scenario("an invalid appointment creation - false enabled") {
+            lateinit var event: CreateAppointmentForTheScheduleEvent
+            var entity: Appointment? = null
+            lateinit var assertion: AbstractThrowableAssert<*, out Throwable>
+            Given("an invalid ${CreateAppointmentForTheScheduleEvent::class.java} instantiation is done") {
+                event = CreateAppointmentForTheScheduleEvent(
+                    AppointmentId("01FK96GENJKTN1BYZW6BRHFZFJ"),
+                    ScheduleId("01FK96GENJKTN1BYZW6BRHFZFJ"),
+                    "HDC",
+                    LocalDate.of(2021, 11, 29),
+                    LocalTime.of(14, 0,0),
+                    30,
+                    FREED,
+                    false,
+                    LocalDateTime.now(),
+                    AuditLog("who", "what")
+                )
+            }
+            When("a unsuccessful appointment instantiation is done") {
+                assertion = assertThatCode {
+                    entity = Appointment.apply(event)
+                }
+            }
+            Then("""instantiation throws exception""") {
+                assertion.hasSameClassAs(AssertionFailedException(MESSAGE_2, ENTITY_APPOINTMENT_ENABLED_MUST_BE_TRUE.code))
+            }
+            And("""exception has code ${ENTITY_APPOINTMENT_ENABLED_MUST_BE_TRUE.code}""") {
+                assertion.hasFieldOrPropertyWithValue("code", ENTITY_APPOINTMENT_ENABLED_MUST_BE_TRUE.code)
+            }
+            And("""exception has message $MESSAGE_2""") {
+                assertion.hasMessage(MESSAGE_2)
+            }
+            And("""no entity was instantiated""") {
+                Assertions.assertThat(entity).isNull()
+            }
+        }
+    }
+})
