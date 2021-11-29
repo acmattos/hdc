6c61c62d8fef082009898ba033063643214b465e
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-11-29 15:40:57 -0300

    adding ContextBuilder

--- src/test/kotlin/br/com/acmattos/hdc/common/tool/server/javalin/ContextBuilder.kt
+++ src/test/kotlin/br/com/acmattos/hdc/common/tool/server/javalin/ContextBuilder.kt
@@ -2,7 +2,12 @@ package br.com.acmattos.hdc.common.tool.server.javalin
 
 import io.javalin.http.Context
 import io.javalin.http.util.ContextUtil
+import io.mockk.every
 import io.mockk.mockk
+import java.io.IOException
+import java.io.InputStream
+import javax.servlet.ReadListener
+import javax.servlet.ServletInputStream
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -14,14 +19,60 @@ object ContextBuilder {
     val req = mockk<HttpServletRequest>()
     val res = mockk<HttpServletResponse>()
 
-    fun mockContext(machedPath: String): Context {
+    fun mockContext(matchedPath: String, jsonBody: String = ""): Context {
         val context: Context = ContextUtil.init(
             req,
             res,
-            machedPath,
-            mapOf(machedPath to "01FJJDJKDXN4K558FMCKEMQE6B")
+            matchedPath,
+            mapOf(matchedPath to "01FJJDJKDXN4K558FMCKEMQE6B")
         )
-
+        every { req.getHeader(any<String>()) } returns "application\\json"
+        every { req.inputStream } returns DelegatingServletInputStream(
+            jsonBody.byteInputStream()
+        )
         return context
+    }
+}
+
+/**
+ * @author ACMattos
+ * @since 27/11/2021.
+ */
+class DelegatingServletInputStream(
+    sourceStream: InputStream
+): ServletInputStream() {
+    /**
+     * Return the underlying source stream (never `null`).
+     */
+    private val sourceStream: InputStream
+
+    /**
+     * Create a DelegatingServletInputStream for the given source stream.
+     * sourceStream the source stream (never `null`)
+     */
+    init {
+        this.sourceStream = sourceStream
+    }
+
+    @Throws(IOException::class)
+    override fun read(): Int {
+        return sourceStream.read()
+    }
+
+    override fun isFinished(): Boolean {
+        return true
+    }
+
+    override fun isReady(): Boolean {
+        return true
+    }
+
+    override fun setReadListener(readListener: ReadListener?) {
+    }
+
+    @Throws(IOException::class)
+    override fun close() {
+        super.close()
+        sourceStream.close()
     }
 }
