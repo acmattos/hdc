a71cbc869147ca114c69a4a4e8f7ab57252a9cb2
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-12-13 14:07:06 -0300

    adding OverlappingAppointmentCriteria

---
+++ src/main/kotlin/br/com/acmattos/hdc/scheduler/port/persistence/mongodb/OverlappingAppointmentCriteria.kt
@@ -0,0 +1,28 @@
+package br.com.acmattos.hdc.scheduler.port.persistence.mongodb
+
+import br.com.acmattos.hdc.common.context.port.persistence.mongodb.MdbCriteria
+import br.com.acmattos.hdc.scheduler.domain.model.ScheduleId
+import com.mongodb.client.model.Filters
+import java.time.LocalDate
+import java.time.LocalTime
+import org.bson.conversions.Bson
+
+/**
+ * @author ACMattos
+ * @since 21/11/2021.
+ */
+data class OverlappingAppointmentCriteria(
+    val scheduleId: ScheduleId,
+    val date: LocalDate,
+    val time: LocalTime
+): MdbCriteria {
+    override val filters: Bson
+        get() = buildFilters()
+
+    private fun buildFilters() =
+        Filters.and(
+            Filters.eq("event.schedule_id.id", scheduleId.id), // TODO TRACK THIS FIELD
+            Filters.eq("event.date", date),// TODO TRACK THIS FIELD
+            Filters.eq("event.time", time),// TODO TRACK THIS FIELD
+        )
+}
