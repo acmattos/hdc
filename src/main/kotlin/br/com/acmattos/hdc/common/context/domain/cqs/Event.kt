f6976296f4a2608ef809b5283e60862f54bef61a
Andre Mattos <andre.mattos.trd@c6bank.com>
2021-12-13 14:02:32 -0300

    adding CreateAppointmentForTheScheduleEvent

--- src/main/kotlin/br/com/acmattos/hdc/common/context/domain/cqs/Event.kt
+++ src/main/kotlin/br/com/acmattos/hdc/common/context/domain/cqs/Event.kt
@@ -4,6 +4,7 @@ import br.com.acmattos.hdc.common.context.domain.model.AuditLog
 import br.com.acmattos.hdc.common.context.domain.model.Entity
 import br.com.acmattos.hdc.common.context.domain.model.Id
 import br.com.acmattos.hdc.scheduler.domain.cqs.CreateAScheduleForTheDentistEvent
+import br.com.acmattos.hdc.scheduler.domain.cqs.CreateAppointmentForTheScheduleEvent
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 
@@ -18,6 +19,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo
 )
 @JsonSubTypes(
     JsonSubTypes.Type(value = CreateAScheduleForTheDentistEvent::class, name = "CreateAScheduleForTheDentistEvent"),
+    JsonSubTypes.Type(value = CreateAppointmentForTheScheduleEvent::class, name = "CreateAppointmentForTheScheduleEvent"),
 )
 open class Event(
     open val eventId: EventId,
