1413a734dec0ca611eca60ae7bd9d64b25bc9bdd
Andre Mattos <andre.mattos.trd@c6bank.com>
2022-02-02 15:44:41 -0300

    adding ScheduleRequestBuilder

--- src/test/kotlin/br/com/acmattos/hdc/scheduler/port/rest/ScheduleRequestBuilder.kt
+++ src/test/kotlin/br/com/acmattos/hdc/scheduler/port/rest/ScheduleRequestBuilder.kt
@@ -1,10 +1,15 @@
 package br.com.acmattos.hdc.scheduler.port.rest
 
+import br.com.acmattos.hdc.scheduler.domain.cqs.CreateAScheduleForTheDentistCommand
+import br.com.acmattos.hdc.scheduler.domain.cqs.CreateAScheduleForTheDentistEvent
+import br.com.acmattos.hdc.scheduler.domain.model.DentistBuilder
+import br.com.acmattos.hdc.scheduler.domain.model.Schedule
+
 /**
  * @author ACMattos
  * @since 21/10/2021.
  */
-object ScheduleRequestBuilder {
+object ScheduleRequestBuilder { // TODO  Test
     fun buildCreateAScheduleForTheDentistRequest() = CreateAScheduleForTheDentistRequest(
         "12345678901234567890123456",
         listOf(
@@ -15,5 +20,13 @@ object ScheduleRequestBuilder {
             30
             )
         ),
+    )
+
+    fun buildSchedule() = Schedule.apply(
+        CreateAScheduleForTheDentistEvent(
+            (buildCreateAScheduleForTheDentistRequest()
+                .toType() as CreateAScheduleForTheDentistCommand)
+            .copy(dentist = DentistBuilder.build())
+        )
     )
 }
