ad4e96e50356e1af7588cb4ea1c23b8debc3307b
Andre Mattos <andre.mattos.trd@c6bank.com>
2022-03-28 14:09:28 -0300

    adding compound index capabilities

--- src/main/kotlin/br/com/acmattos/hdc/common/context/port/persistence/mongodb/MdbCollection.kt
+++ src/main/kotlin/br/com/acmattos/hdc/common/context/port/persistence/mongodb/MdbCollection.kt
@@ -1,6 +1,7 @@
 package br.com.acmattos.hdc.common.context.port.persistence.mongodb
 
 import com.mongodb.client.MongoCollection
+import com.mongodb.client.model.IndexOptions
 import org.bson.conversions.Bson
 
 /**
@@ -19,6 +20,9 @@ class MdbCollection<T: MdbDocument>(
             mdbCollectionConfig.indexes.forEach {
                 mongoCollection.createIndex(it)
             }
+            mdbCollectionConfig.compoundIndexes.forEach {
+                mongoCollection.createIndex(it.first, it.second)
+            }
         }
 }
 
@@ -29,11 +33,19 @@ class MdbCollection<T: MdbDocument>(
 data class MdbCollectionConfig<T: MdbDocument>(
     val collectionName: String,
     val documentClass: Class<T>,
-    val indexes: MutableList<Bson> = mutableListOf()
+    val indexes: MutableList<Bson> = mutableListOf(),
+    val compoundIndexes: MutableList<Pair<Bson, IndexOptions>> = mutableListOf()
 ) {
     fun addIndexes(vararg bsonDocuments: Bson): MdbCollectionConfig<T> {
         bsonDocuments.forEach {
             indexes.add(it)
+        }
+        return this
+    }
+
+    fun addCompoundIndexes(vararg pairs: Pair<Bson, IndexOptions>): MdbCollectionConfig<T> {
+        pairs.forEach {
+            compoundIndexes.add(it)
         }
         return this
     }
