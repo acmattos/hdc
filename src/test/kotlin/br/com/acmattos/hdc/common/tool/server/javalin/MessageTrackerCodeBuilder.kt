e23f5b40d3fc3ef40bcd0f2760229e285f95d867
Andre Mattos <andre.mattos.trd@c6bank.com>
2022-03-28 13:37:27 -0300

    changing Error to MessageTrackerCodeEnum

--- src/test/kotlin/br/com/acmattos/hdc/common/tool/server/javalin/MessageTrackerCodeBuilder.kt
+++ src/test/kotlin/br/com/acmattos/hdc/common/tool/server/javalin/MessageTrackerCodeBuilder.kt
@@ -4,6 +4,6 @@ package br.com.acmattos.hdc.common.tool.server.javalin
  * @author ACMattos
  * @since 30/10/2021.
  */
-object ErrorTrackerCodeBuilder {
-    fun build() = ErrorTrackerCode("01FK6RXZ7BKTN1BYZW6BRHFZFJ")
+object MessageTrackerCodeBuilder {
+    fun build() = MessageTrackerCode("01FK6RXZ7BKTN1BYZW6BRHFZFJ")
 }
