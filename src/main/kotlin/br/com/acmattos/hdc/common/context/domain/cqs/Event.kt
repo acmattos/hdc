a085797867569c7c147836d5f5e828e4aecd1033
Andre Mattos <andre.mattos.trd@c6bank.com>
2022-03-28 14:07:17 -0300

    adding new procedure events

--- src/main/kotlin/br/com/acmattos/hdc/common/context/domain/cqs/Event.kt
+++ src/main/kotlin/br/com/acmattos/hdc/common/context/domain/cqs/Event.kt
@@ -3,6 +3,9 @@ package br.com.acmattos.hdc.common.context.domain.cqs
 import br.com.acmattos.hdc.common.context.domain.model.AuditLog
 import br.com.acmattos.hdc.common.context.domain.model.Entity
 import br.com.acmattos.hdc.common.context.domain.model.Id
+import br.com.acmattos.hdc.procedure.domain.cqs.CreateDentalProcedureEvent
+import br.com.acmattos.hdc.procedure.domain.cqs.DeleteDentalProcedureEvent
+import br.com.acmattos.hdc.procedure.domain.cqs.UpdateDentalProcedureEvent
 import br.com.acmattos.hdc.scheduler.domain.cqs.CreateAScheduleForTheDentistEvent
 import br.com.acmattos.hdc.scheduler.domain.cqs.CreateAppointmentForTheScheduleEvent
 import com.fasterxml.jackson.annotation.JsonSubTypes
@@ -20,6 +23,9 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo
 @JsonSubTypes(
     JsonSubTypes.Type(value = CreateAScheduleForTheDentistEvent::class, name = "CreateAScheduleForTheDentistEvent"),
     JsonSubTypes.Type(value = CreateAppointmentForTheScheduleEvent::class, name = "CreateAppointmentForTheScheduleEvent"),
+    JsonSubTypes.Type(value = CreateDentalProcedureEvent::class, name = "CreateDentalProcedureEvent"),
+    JsonSubTypes.Type(value = UpdateDentalProcedureEvent::class, name = "UpdateDentalProcedureEvent"),
+    JsonSubTypes.Type(value = DeleteDentalProcedureEvent::class, name = "DeleteDentalProcedureEvent"),
 )
 open class Event(
     open val eventId: EventId,
