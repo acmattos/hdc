be25295a60bc7de57dc6a17d022dcd0f65005f9e
Andre Mattos <andre.mattos.trd@c6bank.com>
2022-03-28 14:04:16 -0300

    changing Error to MessageTrackerCodeEnum

--- src/main/kotlin/br/com/acmattos/hdc/person/port/rest/PersonQueryController.kt
+++ src/main/kotlin/br/com/acmattos/hdc/person/port/rest/PersonQueryController.kt
@@ -1,6 +1,7 @@
 package br.com.acmattos.hdc.person.port.rest
 
 import br.com.acmattos.hdc.common.context.domain.cqs.QueryHandler
+import br.com.acmattos.hdc.common.context.domain.cqs.QueryResult
 import br.com.acmattos.hdc.common.context.domain.model.AuditLog
 import br.com.acmattos.hdc.common.context.port.rest.EndpointLogEnum.ENDPOINT
 import br.com.acmattos.hdc.common.context.port.rest.Request
@@ -8,14 +9,20 @@ import br.com.acmattos.hdc.common.tool.assertion.Assertion
 import br.com.acmattos.hdc.common.tool.loggable.Loggable
 import br.com.acmattos.hdc.common.tool.server.javalin.Response
 import br.com.acmattos.hdc.common.tool.server.javalin.getRequest
-import br.com.acmattos.hdc.person.config.ErrorTrackerCodeEnum.DENTIST_ID_INVALID
+import br.com.acmattos.hdc.person.config.MessageTrackerCodeEnum.DENTIST_ID_INVALID
 import br.com.acmattos.hdc.person.config.PersonLogEnum.DENTIST
 import br.com.acmattos.hdc.person.config.PersonLogEnum.PERSON
+import br.com.acmattos.hdc.person.domain.cqs.FindAllContactTypesQuery
+import br.com.acmattos.hdc.person.domain.cqs.FindAllPersonTypesQuery
 import br.com.acmattos.hdc.person.domain.cqs.FindAllPersonsQuery
+import br.com.acmattos.hdc.person.domain.cqs.FindAllStatesQuery
 import br.com.acmattos.hdc.person.domain.cqs.FindTheDentistQuery
 import br.com.acmattos.hdc.person.domain.cqs.PersonQuery
+import br.com.acmattos.hdc.person.domain.model.ContactType
 import br.com.acmattos.hdc.person.domain.model.Person
 import br.com.acmattos.hdc.person.domain.model.PersonId
+import br.com.acmattos.hdc.person.domain.model.PersonType
+import br.com.acmattos.hdc.person.domain.model.State
 import io.javalin.http.Context
 import io.javalin.plugin.openapi.annotations.HttpMethod
 import io.javalin.plugin.openapi.annotations.OpenApi
@@ -106,6 +113,105 @@ class PersonQueryController(
         )
     }
 
+    @OpenApi(
+        summary = "Find all person types",
+        operationId = "findAllPersonTypes",
+        tags = ["Person"],
+        responses = [
+            OpenApiResponse("200", [OpenApiContent(Response::class)]),
+            OpenApiResponse("404", [OpenApiContent(Response::class)])
+        ],
+        method = HttpMethod.GET
+    )
+    fun findAllPersonTypes(context: Context) {// TODO Test
+        logger.debug(
+            "[{} {}] - Finding all persons types...",
+            PERSON.name,
+            ENDPOINT.name
+        )
+        context.getRequest(::FindAllPersonTypesRequest)
+            .toType(what = context.fullUrl())
+            .also { query ->
+                context.status(HttpStatus.OK_200).json(
+                    Response.create(
+                        context.status(),
+                        QueryResult.build(PersonType.values().toList())
+                    )
+                )
+            }
+        logger.info(
+            "[{} {}] - Finding all persons types: -> !DONE! <-",
+            PERSON.name,
+            ENDPOINT.name
+        )
+    }
+
+    @OpenApi(
+        summary = "Find all states",
+        operationId = "findAllStates",
+        tags = ["Person"],
+        responses = [
+            OpenApiResponse("200", [OpenApiContent(Response::class)]),
+            OpenApiResponse("404", [OpenApiContent(Response::class)])
+        ],
+        method = HttpMethod.GET
+    )
+    fun findAllStates(context: Context) {// TODO Test
+        logger.debug(
+            "[{} {}] - Finding all states...",
+            PERSON.name,
+            ENDPOINT.name
+        )
+        context.getRequest(::FindAllStatesRequest)
+            .toType(what = context.fullUrl())
+            .also { query ->
+                context.status(HttpStatus.OK_200).json(
+                    Response.create(
+                        context.status(),
+                        QueryResult.build(State.values().toList())
+                    )
+                )
+            }
+        logger.info(
+            "[{} {}] - Finding all states: -> !DONE! <-",
+            PERSON.name,
+            ENDPOINT.name
+        )
+    }
+
+    @OpenApi(
+        summary = "Find all contact types",
+        operationId = "findAllContactTypes",
+        tags = ["Person"],
+        responses = [
+            OpenApiResponse("200", [OpenApiContent(Response::class)]),
+            OpenApiResponse("404", [OpenApiContent(Response::class)])
+        ],
+        method = HttpMethod.GET
+    )
+    fun findAllContactTypes(context: Context) {// TODO Test
+        logger.debug(
+            "[{} {}] - Finding all contact types...",
+            PERSON.name,
+            ENDPOINT.name
+        )
+        context.getRequest(::FindAllContactTypesRequest)
+            .toType(what = context.fullUrl())
+            .also { query ->
+                context.status(HttpStatus.OK_200).json(
+                    Response.create(
+                        context.status(),
+                        QueryResult.build(ContactType.values().toList())
+                    )
+                )
+            }
+        logger.info(
+            "[{} {}] - Finding all contact types: -> !DONE! <-",
+            PERSON.name,
+            ENDPOINT.name
+        )
+    }
+
     companion object: Loggable()
 }
 
@@ -138,6 +244,42 @@ class FindTheDentistRequest(val context: Context): Request<PersonQuery>(context)
 
         return FindTheDentistQuery(
             PersonId(dentistId),
+            AuditLog(who, what)
+        )
+    }
+}
+
+/**
+ * @author ACMattos
+ * @since 23/02/2022.
+ */
+class FindAllPersonTypesRequest(val context: Context): Request<PersonQuery>(context) {
+    override fun toType(who: String, what: String): PersonQuery {
+        return FindAllPersonTypesQuery(
+            AuditLog(who, what)
+        )
+    }
+}
+
+/**
+ * @author ACMattos
+ * @since 10/03/2022.
+ */
+class FindAllStatesRequest(val context: Context): Request<PersonQuery>(context) {
+    override fun toType(who: String, what: String): PersonQuery {
+        return FindAllStatesQuery(
+            AuditLog(who, what)
+        )
+    }
+}
+
+/**
+ * @author ACMattos
+ * @since 10/03/2022.
+ */
+class FindAllContactTypesRequest(val context: Context): Request<PersonQuery>(context) {
+    override fun toType(who: String, what: String): PersonQuery {
+        return FindAllContactTypesQuery(
             AuditLog(who, what)
         )
     }
