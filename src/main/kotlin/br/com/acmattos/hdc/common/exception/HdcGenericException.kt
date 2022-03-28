67b41be5cd466021f8697b93966243981ddc5720
Andre Mattos <andre.mattos.trd@c6bank.com>
2022-03-28 13:35:01 -0300

    changing Error to MessageTrackerCodeEnum

--- src/main/kotlin/br/com/acmattos/hdc/common/exception/HdcGenericException.kt
+++ src/main/kotlin/br/com/acmattos/hdc/common/exception/HdcGenericException.kt
@@ -1,13 +1,13 @@
 package br.com.acmattos.hdc.common.exception
 
-import br.com.acmattos.hdc.common.tool.server.javalin.ErrorTrackerCode
+import br.com.acmattos.hdc.common.tool.server.javalin.MessageTrackerCode
 
 /**
  * @author ACMattos
  * @since 26/10/2021.
  */
 open class HdcGenericException : RuntimeException {
-    constructor(message: String?, code: ErrorTrackerCode): super(message)
-    constructor(message: String?, code: ErrorTrackerCode, throwable: Throwable):
+    constructor(message: String?, code: MessageTrackerCode): super(message)
+    constructor(message: String?, code: MessageTrackerCode, throwable: Throwable):
         super(message, throwable)
 }
