0065e0ca6e63664870b2b804a43ec3bc9c72c6f9
Andre Mattos <andre.mattos.trd@c6bank.com>
2022-04-07 18:43:35 -0300

    adding regex filter

--- src/main/kotlin/br/com/acmattos/hdc/common/context/port/persistence/mongodb/MdbFilterTranslator.kt
+++ src/main/kotlin/br/com/acmattos/hdc/common/context/port/persistence/mongodb/MdbFilterTranslator.kt
@@ -5,8 +5,14 @@ import br.com.acmattos.hdc.common.context.domain.cqs.EmptyFilter
 import br.com.acmattos.hdc.common.context.domain.cqs.EqFilter
 import br.com.acmattos.hdc.common.context.domain.cqs.Filter
 import br.com.acmattos.hdc.common.context.domain.cqs.FilterTranslator
+import br.com.acmattos.hdc.common.context.domain.cqs.RegexFilter
 import com.mongodb.client.model.Filters
+import java.util.regex.Pattern
 import org.bson.conversions.Bson
+
+private val X_TERM_X = Pattern.compile("(.*)\\w(.*)").toRegex()
+private val X_TERM = Pattern.compile("(.*)\\w").toRegex()
+private val TERM_X = Pattern.compile("\\w(.*)").toRegex()
 
 /**
  * @author ACMattos
@@ -16,12 +22,50 @@ class MdbFilterTranslator: FilterTranslator<Bson> {
     override fun createTranslation(filter: Filter<Bson>): Bson =
         when(filter){
             is EqFilter<Bson, *> -> translate(filter)
+            is RegexFilter<Bson, *> -> translate(filter)
             is AndFilter<Bson> -> translate(filter)
             else -> translate(filter as EmptyFilter)
         }
 
     private fun translate(filter: EqFilter<Bson, *>): Bson =
         Filters.eq(filter.fieldName, filter.value)
+
+    private fun translate(filter: RegexFilter<Bson, *>): Bson =
+       if(!filter.value.toString().contains("*")) {
+            Filters.eq(filter.fieldName, filter.value.toString())
+       } else if(filter.value.toString().matches(TERM_X)) {
+            Filters.eq(
+                filter.fieldName,
+                "/^${filter.value.toString().replace("*", "")}/"
+            )
+        } else if(filter.value.toString().matches(X_TERM)) {
+            Filters.eq(
+                filter.fieldName,
+                "/${filter.value.toString().replace("*", "")}\$/"
+            )
+        } else if(filter.value.toString().matches(X_TERM_X)) {
+            Filters.eq(
+                filter.fieldName,
+                "/.*${filter.value.toString().replace("*", "")}.*/"
+            )
+        } else {
+           Filters.empty()
+        }
+//        MySQL - SELECT * FROM users  WHERE name LIKE '%m%'
+//        MongoDb
+//        1) db.users.find({ "name": { "$regex": "m", "$options": "i" } })
+//        2) db.users.find({ "name": { $regex: new RegExp("m", 'i') } })
+//        3) db.users.find({ "name": { $regex:/m/i } })
+//        4) db.users.find({ "name": /mail/ })
+//        5) db.users.find({ "name": /.*m.*/ })
+//
+//        MySQL - SELECT * FROM users  WHERE name LIKE 'm%'
+//        MongoDb Any of Above with /^String/
+//        6) db.users.find({ "name": /^m/ })
+//
+//        MySQL - SELECT * FROM users  WHERE name LIKE '%m'
+//        MongoDb Any of Above with /String$/
+//        7) db.users.find({ "name": /m$/ })
 
     private fun translate(filter: AndFilter<Bson>): Bson =
         Filters.and(
--- src/test/kotlin/br/com/acmattos/hdc/common/context/port/persistence/mongodb/MdbFilterTranslatorTest.kt
+++ src/test/kotlin/br/com/acmattos/hdc/common/context/port/persistence/mongodb/MdbFilterTranslatorTest.kt
@@ -5,6 +5,7 @@ import br.com.acmattos.hdc.common.context.domain.cqs.EmptyFilter
 import br.com.acmattos.hdc.common.context.domain.cqs.EqFilter
 import br.com.acmattos.hdc.common.context.domain.cqs.Filter
 import br.com.acmattos.hdc.common.context.domain.cqs.FilterTranslator
+import br.com.acmattos.hdc.common.context.domain.cqs.RegexFilter
 import com.mongodb.client.model.Filters
 import org.assertj.core.api.Assertions.assertThat
 import org.bson.conversions.Bson
@@ -13,6 +14,10 @@ import org.spekframework.spek2.style.gherkin.Feature
 
 private const val FIELD_NAME = "fieldName"
 private const val VALUE = 10
+private const val X_VALUE_X = "*value*"
+private const val X_VALUE = "*value"
+private const val VALUE_X = "value*"
+
 /**
  * @author ACMattos
  * @since 01/04/2022.
@@ -107,6 +112,122 @@ object MdbFilterTranslatorTest: Spek({
             }
             And("""the #toString representation matches the expected""") {
                 assertThat(bson.toString()).isEqualTo("{}")
+            }
+        }
+
+        Scenario("${RegexFilter::class.java.simpleName} usage: $X_VALUE_X match") {
+            lateinit var translator: FilterTranslator<Bson>
+            lateinit var filter: Filter<Any>
+            lateinit var fieldName: String
+            lateinit var bson: Any
+            lateinit var value: String
+            Given("""a field named $FIELD_NAME""") {
+                fieldName = FIELD_NAME
+            }
+            And("""a value equals to $X_VALUE_X""") {
+                value = X_VALUE_X
+            }
+            And("""a ${RegexFilter::class.java.simpleName} filter""") {
+                filter = RegexFilter(fieldName, value)
+            }
+            And("""a ${MdbFilterTranslator::class.java.simpleName} filter translator""") {
+                translator = MdbFilterTranslator()
+            }
+            When("""filter#translate is executed""") {
+                bson = filter.translate(translator as FilterTranslator<Any>)
+            }
+            Then("""the filter is equal to a ${Bson::class.java.simpleName} equivalent""") {
+                assertThat(bson).isEqualTo(Filters.eq("fieldName", "/.*value.*/"))
+            }
+            And("""the #toString representation matches the expected""") {
+                assertThat(bson.toString()).isEqualTo("Filter{fieldName='fieldName', value=/.*value.*/}")
+            }
+        }
+
+        Scenario("${RegexFilter::class.java.simpleName} usage: $X_VALUE match") {
+            lateinit var translator: FilterTranslator<Bson>
+            lateinit var filter: Filter<Any>
+            lateinit var fieldName: String
+            lateinit var bson: Any
+            lateinit var value: String
+            Given("""a field named $FIELD_NAME""") {
+                fieldName = FIELD_NAME
+            }
+            And("""a value equals to $X_VALUE""") {
+                value = X_VALUE
+            }
+            And("""a ${RegexFilter::class.java.simpleName} filter""") {
+                filter = RegexFilter(fieldName, value)
+            }
+            And("""a ${MdbFilterTranslator::class.java.simpleName} filter translator""") {
+                translator = MdbFilterTranslator()
+            }
+            When("""filter#translate is executed""") {
+                bson = filter.translate(translator as FilterTranslator<Any>)
+            }
+            Then("""the filter is equal to a ${Bson::class.java.simpleName} equivalent""") {
+                assertThat(bson).isEqualTo(Filters.eq("fieldName", "/value\$/"))
+            }
+            And("""the #toString representation matches the expected""") {
+                assertThat(bson.toString()).isEqualTo("Filter{fieldName='fieldName', value=/value\$/}")
+            }
+        }
+
+        Scenario("${RegexFilter::class.java.simpleName} usage: $VALUE_X match") {
+            lateinit var translator: FilterTranslator<Bson>
+            lateinit var filter: Filter<Any>
+            lateinit var fieldName: String
+            lateinit var bson: Any
+            lateinit var value: String
+            Given("""a field named $FIELD_NAME""") {
+                fieldName = FIELD_NAME
+            }
+            And("""a value equals to $VALUE_X""") {
+                value = VALUE_X
+            }
+            And("""a ${RegexFilter::class.java.simpleName} filter""") {
+                filter = RegexFilter(fieldName, value)
+            }
+            And("""a ${MdbFilterTranslator::class.java.simpleName} filter translator""") {
+                translator = MdbFilterTranslator()
+            }
+            When("""filter#translate is executed""") {
+                bson = filter.translate(translator as FilterTranslator<Any>)
+            }
+            Then("""the filter is equal to a ${Bson::class.java.simpleName} equivalent""") {
+                assertThat(bson).isEqualTo(Filters.eq("fieldName", "/^value/"))
+            }
+            And("""the #toString representation matches the expected""") {
+                assertThat(bson.toString()).isEqualTo("Filter{fieldName='fieldName', value=/^value/}")
+            }
+        }
+
+        Scenario("${RegexFilter::class.java.simpleName} usage: exact match") {
+            lateinit var translator: FilterTranslator<Bson>
+            lateinit var filter: Filter<Any>
+            lateinit var fieldName: String
+            lateinit var bson: Any
+            lateinit var value:String
+            Given("""a field named $FIELD_NAME""") {
+                fieldName = FIELD_NAME
+            }
+            And("""a value equals to $VALUE""") {
+                value = "" + VALUE
+            }
+            And("""a ${RegexFilter::class.java.simpleName} filter""") {
+                filter = RegexFilter(fieldName, value+"")
+            }
+            And("""a ${MdbFilterTranslator::class.java.simpleName} filter translator""") {
+                translator = MdbFilterTranslator()
+            }
+            When("""filter#translate is executed""") {
+                bson = filter.translate(translator as FilterTranslator<Any>)
+            }
+            Then("""the filter is equal to a ${Bson::class.java.simpleName} equivalent""") {
+                assertThat(bson).isEqualTo(Filters.eq(fieldName, "" + VALUE))
+            }
+            And("""the #toString representation matches the expected""") {
+                assertThat(bson.toString()).isEqualTo("Filter{fieldName='fieldName', value=10}")
             }
         }
     }
