292b20034451a77525cb1a08d52a44481241a9b9
Andre Mattos <andre.mattos.trd@c6bank.com>
2022-04-14 18:19:20 -0300

    making PageResult inherit from QueryResult

--- src/main/kotlin/br/com/acmattos/hdc/common/tool/page/Page.kt
+++ src/main/kotlin/br/com/acmattos/hdc/common/tool/page/Page.kt
@@ -2,6 +2,7 @@ package br.com.acmattos.hdc.common.tool.page
 
 import br.com.acmattos.hdc.common.context.domain.cqs.EmptyFilter
 import br.com.acmattos.hdc.common.context.domain.cqs.Filter
+import br.com.acmattos.hdc.common.context.domain.cqs.QueryResult
 
 /**
  * @author ACMattos
@@ -21,15 +22,20 @@ data class Page private constructor(
         }
 
     companion object {
-        private const val DEFAULT_SIZE = 10
-        private const val FIRST_PAGE = 0
+        private const val DEFAULT_SIZE = "10"//10
+        internal const val FIRST_PAGE = 0
 
         fun create(
             filter: Filter<*> = EmptyFilter(),
             sort: Sort<*> = CollectionSort<Any>(),
             number: String? = null,
-            size: Int = DEFAULT_SIZE
-        ) = Page(filter, sort, number?.toInt() ?: FIRST_PAGE, size)
+            size: String? = DEFAULT_SIZE
+        ) = Page(
+            filter,
+            sort,
+            number?.toIntOrDefault(FIRST_PAGE) ?: FIRST_PAGE,
+            20
+        )
     }
 }
 
@@ -38,15 +44,26 @@ data class Page private constructor(
  * @since 05/04/2022.
  */
 data class PageResult<R> private constructor(
-    val results: List<R>,
+    override val results: List<R>,
     val page: Page,
     val total: Long
-) {
+): QueryResult<R>(results) {
     companion object {
         fun <R> create(
             results: List<R>,
             page: Page,
-           total: Long
+            total: Long
         ) = PageResult(results, page, total)
     }
 }
+
+/**
+ * @author ACMattos
+ * @since 13/04/2022.
+ */
+private fun String.toIntOrDefault(default: Int): Int =
+    try {
+        this.toInt()
+    } catch (ex: Exception) {
+        default
+    }
