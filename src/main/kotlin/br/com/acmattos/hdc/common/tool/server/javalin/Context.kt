f62acf615f3a793ce617d11620919b14a10f9304
Andre Mattos <andre.mattos.trd@c6bank.com>
2022-04-14 18:12:25 -0300

    adding #pageSize

--- src/main/kotlin/br/com/acmattos/hdc/common/tool/server/javalin/Context.kt
+++ src/main/kotlin/br/com/acmattos/hdc/common/tool/server/javalin/Context.kt
@@ -19,10 +19,16 @@ inline fun <reified T: Request<*>> Context.getRequest(
  */
 inline fun Context.filterParam(
     name: String
-): String?  = this.queryParam("f_$name")
+): String? = this.queryParam("f_$name")
 
 /**
  * @author ACMattos
  * @since 11/04/2022.
  */
 inline fun Context.pageNumber(): String? = this.queryParam("pn")
+
+/**
+ * @author ACMattos
+ * @since 13/04/2022.
+ */
+inline fun Context.pageSize(): String? = this.queryParam("ps")
