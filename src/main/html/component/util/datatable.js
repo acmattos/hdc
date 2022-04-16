e3a1f874a1133ed45195d84494485daf2be98f5d
Andre Mattos <andre.mattos.trd@c6bank.com>
2022-04-15 23:02:36 -0300

    improving datatables configurations

--- src/main/html/component/util/datatable.js
+++ src/main/html/component/util/datatable.js
@@ -11,13 +11,30 @@
       config() {
          let initComplete = this.initComplete;
          return {
-             // 'processing': true,
-             // 'serverSide': true,
+            'processing': true,
+            'serverSide': true,
+            'stateSave': true,
             'ajax': {
                'url': resource.getFullUrl(this.uri),
                'type': 'GET',
                'dataType': 'json',
-               'dataSrc': 'data.results',
+               'dataFilter': function(data) {
+                  let json = jQuery.parseJSON( data );
+                  json.recordsTotal = json.data.total;
+                  json.recordsFiltered = json.data.total;
+                  json.data = json.data.results;
+                  return JSON.stringify(json);
+               },
+               'data': function(data, settings) {
+                  let api = new $.fn.dataTable.Api(settings);
+                  data.pn = Math.min(
+                     Math.max(0, Math.round(data.start / api.page.len())),
+                     api.page.info().pages
+                  ) + 1 ;
+                  data.ps = api.page.len();
+                  $.inputText ('#pageSize', data.ps);
+                  $.inputText('#pageNumber', data.pn);
+               },
                'error': function(jqXHR, textStatus, errorThrown) {
                   toast.show({'status': 499 ,
                      'code': '01FVQ2NP5N57RMXBZDKHVT7ZGJ',
@@ -25,25 +42,38 @@
                   });
                }
             },
+            'lengthMenu': [[2,10, 25, 50, -1], [2,10, 25, 50, "Todos"]],
             'columns': this.columns,
             'columnDefs': this.columnDefs,
             'initComplete': function(settings, json) {
                initComplete(settings, json);
             },
             'language': {
-               'emptyTable': 'Tabela vazia',
-               'zeroRecords': 'Nenhum resultado encontrado',
-               'processing': 'Carregando...'//TODO USE loader
+               'processing':     'Carregando...',
+               'search'    :     'Busca',
+               'lengthMenu':     'Mostrar _MENU_ registros',
+               'info'      :     'Exibindo _START_ a _END_ de _TOTAL_ resultados',
+               'infoEmpty' :     'Exibindo 0 a 0 de 0 resultados',
+               'loadingRecords': 'Carregando registros...',
+               'emptyTable':     'Sem resultados',
+               'zeroRecords':    'Nenhum resultado encontrado',
+               'paginate': {
+                   'first':      'Primeiro',
+                   'previous':   'Prév.',
+                   'next':       'Próx.',
+                   'last':       'Último'
+                },
             },
-            'error': 'Erro na leitura!',
+            'error':          'Erro na leitura!',
             'order':          [],
             'scrollY':        '72vh',
             'deferRender':    true,
             'paging':         true,
+            'searching':      false,
             'scrollCollapse': true,
-            'filter':         false,
-            'info':           false,
-            'lengthChange':   false
+            'filter':         true,
+            'info':           true,
+            'lengthChange':   true
          }
       }
    }
@@ -54,7 +84,13 @@
          this.dtConfig = dtConfig;
       }
       table() {
-         return $(this.component).DataTable(this.dtConfig.config());
+         let component = $(this.component);
+         if($.fn.dataTable.isDataTable(component)){
+            component.DataTable().destroy();
+            component.find("tbody tr:gt(0)").remove();
+         }
+
+         return component.DataTable(this.dtConfig.config());
       }
    }
    window.Datatable = Datatable;
