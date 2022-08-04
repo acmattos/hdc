67a3c09c15833171506407c8281930b0a3788846
Andre Mattos <andre.mattos.trd@c6bank.com>
2022-08-04 17:47:08 -0300

    adding extra extensions

--- src/main/html/component/util/jquery-extensions.js
+++ src/main/html/component/util/jquery-extensions.js
@@ -48,6 +48,57 @@
       }
    };
 
+   $.inputDate = (component, value, minDate, maxDate) => {
+      let mask = 'Dd/Mm/YYYY';
+      let maskOptions = {
+         onInvalid: (val, e, f, invalid, options) => {
+            var error = invalid[0];
+            logger.info("Digit: " + error.v + " is invalid for the position: " +
+               error.p + ". We expect something like: " + error.e);
+         },
+         translation: {
+            'M': {pattern: /[0-1]/},
+            'm': {pattern: /[0-9]/},
+            'D': {pattern: /[0-3]/},
+            'd': {pattern: /[0-9]/},
+            'Y': {pattern: /[0-9]/},
+            '/': {pattern: /[\/]/, fallback: '/'}
+         },
+         reverse: false
+      };
+      let datepickerConfig = {
+         constrainInput: true,
+         changeMonth: true,
+         changeYear: true,
+         showAnim: 'fade',
+         dateFormat: 'dd/mm/yy',
+         dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta',
+            'Sábado', 'Domingo'],
+         dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S', 'D'],
+         dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom'],
+         monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+            'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+         monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago',
+            'Set', 'Out', 'Nov', 'Dez']
+      };
+      if (minDate) {
+         datepickerConfig.minDate = minDate;
+      }
+      if(maxDate) {
+         datepickerConfig.maxDate = maxDate;
+      }
+      if(!value) {
+         let text = $(component).val();
+         $.maskInputText(component, mask, maskOptions);
+         $(component).datepicker(datepickerConfig);
+         return !text ? '' : text.toLocalDate();
+      } else {
+         $(component).val(value.fromLocalDate());
+         $.maskInputText(component, mask, maskOptions);
+         $(component).datepicker(datepickerConfig);
+      }
+   }
+
    $.text = (component, value) => {
       $(component).text(value);
    };
@@ -84,12 +135,24 @@
       }
    };
 
+   $.keyup = (component, length, action) => {
+      $(component).off('keyup').on('keyup', (event) => {
+         if($.inputText(component).length > length) {
+            action();
+         }
+      });
+   };
+
    $.disabled = (component, disabled) => {
       $(component).prop('disabled', disabled);
    }
 
-   $.trigger = (component) => {
-      $(component).trigger('click');
+   $.trigger = (component, value) => {
+     if(!value) {
+        $(component).trigger('click');
+     } else {
+        $(component).trigger(component + '-valid', value);
+     }
    }
 
    //values = [{"id":"id_here", "text":"text_here"}]
@@ -128,104 +191,5 @@
       });
    }
 
-   // var inicializaSelect = (idComponente, data, id) => {
-   //    $(idComponente + " option[value!='']").each((index, element) => {
-   //       if(element.value) {
-   //          $(element).remove();
-   //       }
-   //    });
-   //    var option = '', val1 = '', val2 = '', selected;
-   //    $.each(data, (index, elem) => {
-   //       selected = '';
-   //       if(idComponente.includes('anormalidadeTipoTO')
-   //          || (idComponente.includes('operacao')
-   //             && !idComponente.includes('operacaoExecucao'))){
-   //          val1 = JSON.stringify(elem).replace(/'/g, '');
-   //          val2 = elem.nome;
-   //          if(elem.id == id) {
-   //             selected = 'SELECTED';
-   //          }
-   //       } else {
-   //          val1 = elem;
-   //          val2 = elem;
-   //          if(elem === id) {
-   //             selected = 'SELECTED';
-   //          }
-   //       }
-   //       option += "<option value='" + val1  + "' " + selected
-   //          + ">" + val2 + "</option>";
-   //    });
-   //    $(idComponente).append(option);
-   //    $(idComponente).trigger(idComponente + '-loaded');
-   //    $(idComponente).off('change').change((event) => {
-   //       var valor = $(idComponente + ' option:selected').val();
-   //       if(valor) {
-   //          valor = valor.includes('{') ? JSON.parse(valor) : valor;
-   //          $(idComponente).trigger(idComponente + '-valid', valor);
-   //       } else {
-   //          $(idComponente).trigger(idComponente + '-invalid');
-   //       }
-   //    });
-   //    if($(idComponente + ' option').length === 1) {
-   //       $(idComponente).prop('disabled', true);
-   //    } else if($(idComponente + ' option').length === 2) {
-   //       $(idComponente + ' :nth-child(2)').prop('selected', true);
-   //       $(idComponente).trigger('change');
-   //    }
-   // }
-   //
-   // var inicializaSelectEscravo = (idComponenteMestre, idComponenteEscravo,
-   //                                callbackComponenteEscravo) => {
-   //    $('body')
-   //       .off(idComponenteMestre + '-valid')
-   //       .on(idComponenteMestre + '-valid', (event, selector, data) => {
-   //          if(idComponenteMestre === '#setorOperacionalTO'){
-   //             $("#rotaTO option:first").attr('selected', 'selected');
-   //             $('#rotaTO').prop('disabled', true);
-   //             $('#rotaTO').trigger('#rotaTO-invalid');
-   //          } else if (idComponenteMestre === '#rotaTO') {
-   //             $("#setorOperacionalTO option:first").attr('selected', 'selected');
-   //             $('#setorOperacionalTO').prop('disabled', true);
-   //             $('#setorOperacionalTO').trigger('#setorOperacionalTO-invalid');
-   //          } else if(idComponenteMestre === '#paSetorOperacionalTO'){
-   //             $("#paRotaTO option:first").attr('selected', 'selected');
-   //             $('#paRotaTO').prop('disabled', true);
-   //             $('#paRotaTO').trigger('#paRotaTO-invalid');
-   //          } else if (idComponenteMestre === '#paRotaTO') {
-   //             $("#paSetorOperacionalTO option:first").attr('selected','selected');
-   //             $('#paSetorOperacionalTO').prop('disabled', true);
-   //             $('#paSetorOperacionalTO').trigger('#paSetorOperacionalTO-invalid');
-   //          }
-   //
-   //          $.when(callbackComponenteEscravo(selector))
-   //             .done((tos) => {
-   //                $('body')
-   //                   .off(idComponenteEscravo + '-loaded')
-   //                   .on(idComponenteEscravo + '-loaded', (event, selector, data) => {
-   //                      $(idComponenteEscravo).prop('disabled',false);
-   //                   });
-   //                inicializaSelect(idComponenteEscravo, tos);
-   //             });
-   //       })
-   //       .off(idComponenteMestre + '-invalid')
-   //       .on(idComponenteMestre + '-invalid', (event) => {
-   //          if(idComponenteMestre === '#setorOperacionalTO'){
-   //             $('#rotaTO').prop('disabled', '');
-   //          } else if (idComponenteMestre === '#rotaTO') {
-   //             $('#setorOperacionalTO').prop('disabled', false);
-   //          } else if(idComponenteMestre === '#paSetorOperacionalTO'){
-   //             $('#paRotaTO').prop('disabled', '');
-   //          } else if (idComponenteMestre === '#paRotaTO') {
-   //             $('#paSetorOperacionalTO').prop('disabled', false);
-   //          }
-   //          $(idComponenteEscravo).prop('disabled',true);
-   //          $(idComponenteEscravo + " option[value!='']").each((index, element) => {
-   //             if(element.value) {
-   //                $(element).remove();
-   //             }
-   //          });
-   //          $(idComponenteEscravo).trigger(idComponenteEscravo + '-invalid');
-   //       });
-   // }
 
 })();
