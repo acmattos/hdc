77cb16c832cf0d33ceb3c12b7955b4c63235b2f6
Andre Mattos <andre.mattos.trd@c6bank.com>
2022-08-04 17:55:04 -0300

    adding DocumentIndexedField

---
+++ src/main/kotlin/br/com/acmattos/hdc/person/port/persistence/mongodb/DocumentIndexedField.kt
@@ -0,0 +1,16 @@
+package br.com.acmattos.hdc.person.port.persistence.mongodb
+
+/**
+ * @author ACMattos
+ * @since 23/05/2022.
+ */
+enum class DocumentIndexedField(val fieldName: String) {
+    EVENT_PERSON_ID_ID("event.person_id.id"),
+    CPF("cpf"),
+    FULL_NAME("full_name"),
+    PERSON_ID("person_id"),
+    CONTACTS_INFO("contacts.info"),
+    DENTAL_PLAN_NAME("dental_plan.name"),
+    QUERY_ID_ID("query.id.id"),
+    QUERY_AUDIT_LOG_WHO("query.audit_log.who"),
+}
