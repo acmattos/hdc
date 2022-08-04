69d256019f75f9d561af502538a187f7e5b8a911
Andre Mattos <andre.mattos.trd@c6bank.com>
2022-08-04 17:46:07 -0300

    adding modal close listener

--- src/main/html/index.js
+++ src/main/html/index.js
@@ -5,10 +5,13 @@
       $.when(
          resource.script('component/toast/toast'),
          resource.script('component/util/validator'),
-         //resource.component('#workspace', 'restricted/person/patient/patient-list')
-         resource.component('#workspace', 'restricted/procedure/procedure-list')
+         resource.component('#workspace', 'restricted/person/patient/patient-list')
+         //resource.component('#workspace', 'restricted/procedure/procedure-list')
       ).done(() => {
          new Logger("index.js").info('HDC is now loaded.', window.location);
+         $('#modalCloseButton').off('click').on('click', (event) => {
+            $.trigger('#modalCloseButton', 'close');
+         });
       });
    });
 })();
