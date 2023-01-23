84f462166a1e622725fbaaba3fbd252ae19ab036
Andre Mattos <andre.mattos.trd@c6bank.com>
2023-01-23 14:29:54 -0300

    Changing event name

--- src/test/kotlin/br/com/acmattos/hdc/procedure/port/rest/ProcedureRequestBuilder.kt
+++ src/test/kotlin/br/com/acmattos/hdc/procedure/port/rest/ProcedureRequestBuilder.kt
@@ -7,41 +7,45 @@ import br.com.acmattos.hdc.common.tool.server.javalin.ContextBuilder
  * @since 20/03/2022.
  */
 object ProcedureRequestBuilder {
-    fun buildCreateDentalProcedureRequest() = CreateDentalProcedureRequest(
+    fun buildCreateDentalProcedureRequest() = ProcedureCreateRequest(
         81000014,
         "Procedure Description",
     )
 
-    fun buildCreateDentalProcedureRequestInvalidCode() = CreateDentalProcedureRequest(
+    fun buildCreateDentalProcedureRequestInvalidCode() = ProcedureCreateRequest(
         8100001,
         "Procedure Description",
     )
 
-    fun buildCreateDentalProcedureRequestInvalidDescription() = CreateDentalProcedureRequest(
+    fun buildCreateDentalProcedureRequestInvalidDescription() = ProcedureCreateRequest(
         81000014,
         "Pr",
     )
 
-    fun buildUpdateDentalProcedureRequest() = UpdateDentalProcedureRequest(
+    fun buildUpdateDentalProcedureRequest() = ProcedureUpdateRequest(
         "12345678901234567890123456",
         81000014,
         "Procedure Description",
         true
     )
 
-    fun buildDeleteDentalProcedureRequest() = DeleteDentalProcedureRequest(
-        getContext()
+    fun buildDeleteDentalProcedureRequest() = ProcedureDeleteRequest(
+        getContext(
+            matchedPath = "procedure_ids",
+            matchedPathValue = "01FJJDJKDXN4K558FMCKEMQE6B;1"
+        )
     )
 
     fun buildFindAllProceduresRequest() = FindAllProceduresRequest(
-        getContext()
+        getContext(matchedPath = "procedure_id")
     )
 
     fun buildFindTheProcedureRequest() = FindTheProcedureRequest(
-        getContext()
+        getContext(matchedPath = "procedure_id")
     )
 
-    private fun getContext() = ContextBuilder().mockContext(
-        "procedure_id"
-    )
+    private fun getContext(
+        matchedPath: String = "procedure_id",
+        matchedPathValue: String = "01FJJDJKDXN4K558FMCKEMQE6B"
+    ) = ContextBuilder().mockContext(matchedPath, matchedPathValue)
 }
