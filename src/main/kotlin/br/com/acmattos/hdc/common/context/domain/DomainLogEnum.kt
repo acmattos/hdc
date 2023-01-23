135cbf73df8d674449a0d1a1cfb681c41ff1aa6b
Andre Mattos <andre.mattos.trd@c6bank.com>
2023-01-23 14:34:50 -0300

    removing unused

--- src/main/kotlin/br/com/acmattos/hdc/common/context/domain/DomainLogEnum.kt
+++
@@ -1,9 +0,0 @@
-package br.com.acmattos.hdc.common.context.domain
-
-/**
- * @author ACMattos
- * @since 19/10/2021.
- */
-enum class DomainLogEnum {
-    ENTITY,
-}
--- src/main/kotlin/br/com/acmattos/hdc/common/context/port/rest/EndpointLogEnum.kt
+++
@@ -1,9 +0,0 @@
-package br.com.acmattos.hdc.common.context.port.rest
-
-/**
- * @author ACMattos
- * @since 30/09/2021.
- */
-enum class EndpointLogEnum {
-    ENDPOINT
-}
