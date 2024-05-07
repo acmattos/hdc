73e5eda3d718befa760ecfd2a32758e2bd967e54
ACMattos <acmattos@gmail.com>
2024-05-07 12:44:51 -0300

    adding kotest

--- src/test/kotlin/br/com/acmattos/hdc/common/tool/server/mapper/LocalDateSerializerTest.kt
+++ src/test/kotlin/br/com/acmattos/hdc/common/tool/server/mapper/LocalDateSerializerTest.kt
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializerProvider
+import io.kotest.core.spec.style.FreeSpec
+import org.assertj.core.api.Assertions.assertThat
 import java.io.ByteArrayInputStream
 import java.io.InputStream
 import java.io.StringWriter
@@ -12,9 +14,6 @@ import java.io.Writer
 import java.nio.charset.StandardCharsets
 import java.time.LocalDate
 import java.time.Month.SEPTEMBER
-import org.assertj.core.api.Assertions.assertThat
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.gherkin.Feature
 
 private val LOCAL_DATE = LocalDate.of(2021,SEPTEMBER, 27)
 private const val LOCAL_DATE_STRING = "\"2021-09-27\""
@@ -23,32 +22,32 @@ private const val LOCAL_DATE_STRING = "\"2021-09-27\""
  * @author ACMattos
  * @since 27/09/2021.
  */
-object LocalDateSerializerTest: Spek({
-    Feature("${LocalDateSerializer::class.java} usage") {
-        Scenario("serialization is done successfully") {
+class LocalDateSerializerTest: FreeSpec({
+    "Feature: ${LocalDateSerializer::class.java} usage" - {
+        "Scenario: serialization is done successfully" - {
             lateinit var serializer: LocalDateSerializer
             lateinit var writer: Writer
             lateinit var generator: JsonGenerator
             lateinit var provider: SerializerProvider
-            Given("""a ${LocalDateSerializer::class.java}""") {
+            "Given: a ${LocalDateSerializer::class.java}" {
                 serializer = LocalDateSerializer()
             }
-            And("""a ${StringWriter::class.java}""") {
+            "And: a ${StringWriter::class.java}" {
                 writer = StringWriter()
             }
-            And("""a ${JsonGenerator::class.java}""") {
+            "And: a ${JsonGenerator::class.java}" {
                 generator = JsonFactory().createGenerator(writer)
             }
-            And("""a ${SerializerProvider::class.java}""") {
+            "And: a ${SerializerProvider::class.java}" {
                 provider = JacksonObjectMapperFactory.build().serializerProvider
             }
-            When("""#serialize is executed""") {
+            "When: #serialize is executed" {
                 serializer.serialize(LOCAL_DATE, generator, provider)
             }
-            And("""#flush is executed""") {
+            "And: #flush is executed" {
                 generator.flush()
             }
-            Then("""a mapper is returned""") {
+            "Then: a mapper is returned" {
                 assertThat(writer.toString()).isEqualTo(LOCAL_DATE_STRING)
             }
         }
@@ -59,36 +58,36 @@ object LocalDateSerializerTest: Spek({
  * @author ACMattos
  * @since 27/09/2021.
  */
-object LocalDateDeserializerTest: Spek({
-    Feature("${LocalDateDeserializer::class.java} usage") {
-        Scenario("de-serialization is done successfully") {
+class LocalDateDeserializerTest: FreeSpec({
+    "Feature: ${LocalDateDeserializer::class.java} usage" - {
+        "Scenario: de-serialization is done successfully" - {
             lateinit var deserializer: LocalDateDeserializer
             lateinit var json: String
             lateinit var stream: InputStream
             lateinit var mapper: ObjectMapper
             lateinit var parser: JsonParser
             lateinit var dsValue: LocalDate
-            Given("""a ${LocalDateDeserializer::class.java}""") {
+            "Given: a ${LocalDateDeserializer::class.java}" {
                 deserializer = LocalDateDeserializer()
             }
-            And("""a String representation ($LOCAL_DATE_STRING) of a ${LocalDate::class.java}""") {
+            "And: a String representation ($LOCAL_DATE_STRING) of a ${LocalDate::class.java}" {
                 json = String.format(LOCAL_DATE_STRING)
             }
-            And("""a ${InputStream::class.java} representation of the json string""") {
+            "And: a ${InputStream::class.java} representation of the json string" {
                 stream = ByteArrayInputStream(
                     json.toByteArray(StandardCharsets.UTF_8)
                 )
             }
-            And("""a ${ObjectMapper::class.java}""") {
+            "And: a ${ObjectMapper::class.java}" {
                 mapper = JacksonObjectMapperFactory.build()
             }
-            And("""a ${JsonParser::class.java} for the ${InputStream::class.java}""") {
+            "And: a ${JsonParser::class.java} for the ${InputStream::class.java}" {
                 parser = mapper.factory.createParser(stream)
             }
-            When("""#deserialize is executed""") {
+            "When: #deserialize is executed" {
                 dsValue = deserializer.deserialize(parser, mapper.deserializationContext)
             }
-            Then("""the deserialized value is equal to $LOCAL_DATE""") {
+            "Then: the deserialized value is equal to $LOCAL_DATE" {
                 assertThat(dsValue).isEqualTo(LOCAL_DATE)
             }
         }
--- src/test/kotlin/br/com/acmattos/hdc/common/tool/server/mapper/LocalTimeSerializerTest.kt
+++ src/test/kotlin/br/com/acmattos/hdc/common/tool/server/mapper/LocalTimeSerializerTest.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializerProvider
+import io.kotest.core.spec.style.FreeSpec
 import java.io.ByteArrayInputStream
 import java.io.InputStream
 import java.io.StringWriter
@@ -13,8 +14,6 @@ import java.nio.charset.StandardCharsets
 import java.time.LocalDate
 import java.time.LocalTime
 import org.assertj.core.api.Assertions.assertThat
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.gherkin.Feature
 
 private val LOCAL_TIME = LocalTime.of(20, 27)
 private const val LOCAL_TIME_STRING = "\"20:27\""
@@ -23,32 +22,32 @@ private const val LOCAL_TIME_STRING = "\"20:27\""
  * @author ACMattos
  * @since 27/09/2021.
  */
-object LocalTimeSerializerTest: Spek({
-    Feature("${LocalTimeSerializer::class.java} usage") {
-        Scenario("serialization is done successfully") {
+object LocalTimeSerializerTest: FreeSpec({
+    "Feature: ${LocalTimeSerializer::class.java} usage" - {
+        "Scenario: serialization is done successfully" - {
             lateinit var serializer: LocalTimeSerializer
             lateinit var writer: Writer
             lateinit var generator: JsonGenerator
             lateinit var provider: SerializerProvider
-            Given("""a ${LocalTimeSerializer::class.java}""") {
+            "Given: a ${LocalTimeSerializer::class.java}" {
                 serializer = LocalTimeSerializer()
             }
-            And("""a ${StringWriter::class.java}""") {
+            "And: a ${StringWriter::class.java}" {
                 writer = StringWriter()
             }
-            And("""a ${JsonGenerator::class.java}""") {
+            "And: a ${JsonGenerator::class.java}" {
                 generator = JsonFactory().createGenerator(writer)
             }
-            And("""a ${SerializerProvider::class.java}""") {
+            "And: a ${SerializerProvider::class.java}" {
                 provider = JacksonObjectMapperFactory.build().serializerProvider
             }
-            When("""#serialize is executed""") {
+            "When: #serialize is executed" {
                 serializer.serialize(LOCAL_TIME, generator, provider)
             }
-            And("""#flush is executed""") {
+            "And: #flush is executed" {
                 generator.flush()
             }
-            Then("""a mapper is returned""") {
+            "Then: a mapper is returned" {
                 assertThat(writer.toString()).isEqualTo(LOCAL_TIME_STRING)
             }
         }
@@ -59,36 +58,37 @@ object LocalTimeSerializerTest: Spek({
  * @author ACMattos
  * @since 27/09/2021.
  */
-object LocalTimeDeserializerTest: Spek({
-    Feature("${LocalTimeDeserializer::class.java} usage") {
-        Scenario("de-serialization is done successfully") {
+class LocalTimeDeserializerTest: FreeSpec({
+    "Feature: ${LocalTimeDeserializer::class.java} usage" - {
+        "Scenario: de-serialization is done successfully" - {
             lateinit var deserializer: LocalTimeDeserializer
             lateinit var json: String
             lateinit var stream: InputStream
             lateinit var mapper: ObjectMapper
             lateinit var parser: JsonParser
             lateinit var dsValue: LocalTime
-            Given("""a ${LocalTimeDeserializer::class.java}""") {
+            "Given: a ${LocalTimeDeserializer::class.java}" {
                 deserializer = LocalTimeDeserializer()
             }
-            And("""a String representation ($LOCAL_TIME_STRING) of a ${LocalDate::class.java}""") {
+            "And: a String representation ($LOCAL_TIME_STRING) of a ${LocalDate::class.java}" {
                 json = String.format(LOCAL_TIME_STRING)
             }
-            And("""a ${InputStream::class.java} representation of the json string""") {
+            "And: a ${InputStream::class.java} representation of the json string" {
                 stream = ByteArrayInputStream(
                     json.toByteArray(StandardCharsets.UTF_8)
                 )
             }
-            And("""a ${ObjectMapper::class.java}""") {
+            "And: a ${ObjectMapper::class.java}" {
                 mapper = JacksonObjectMapperFactory.build()
             }
-            And("""a ${JsonParser::class.java} for the ${InputStream::class.java}""") {
+            "And: a ${JsonParser::class.java} for the ${InputStream::class.java}" {
                 parser = mapper.factory.createParser(stream)
             }
-            When("""#deserialize is executed""") {
-                dsValue = deserializer.deserialize(parser, mapper.deserializationContext)
+            "When: #deserialize is executed" {
+                dsValue = deserializer.deserialize(parser,
+                    mapper.deserializationContext)
             }
-            Then("""the deserialized value is equal to $LOCAL_TIME""") {
+            "Then: the deserialized value is equal to $LOCAL_TIME" {
                 assertThat(dsValue).isEqualTo(LOCAL_TIME)
             }
         }
