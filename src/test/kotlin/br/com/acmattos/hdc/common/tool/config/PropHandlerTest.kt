6d647c1b2855f05ad69c4a9b9f7e2afd517730a2
ACMattos <acmattos@gmail.com>
2024-05-07 10:25:56 -0300

    adding kotest

--- src/test/kotlin/br/com/acmattos/hdc/common/tool/config/PropHandlerTest.kt
+++ src/test/kotlin/br/com/acmattos/hdc/common/tool/config/PropHandlerTest.kt
@@ -1,104 +1,103 @@
 package br.com.acmattos.hdc.common.tool.config
 
+import io.kotest.core.spec.style.FreeSpec
 import org.assertj.core.api.AbstractThrowableAssert
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.gherkin.Feature
 
 /**
  * @author ACMattos
  * @since 09/06/2020.
  */
-object PropHandlerTest: Spek({
-    Feature("""${PropHandler::class.java} usage""") {
-        Scenario("""valid key given to #getProperty""") {
+class PropHandlerTest: FreeSpec({
+    "Feature: ${PropHandler::class.java} usage" - {
+        "Scenario: valid key given to #getProperty" - {
             lateinit var key: String
             lateinit var value: String
             lateinit var result: String
 
-            Given("""a valid key named 'key'""") {
+            "Given: a valid key named 'key'" {
                 key = "key"
             }
-            And("""a valid value containing 'value'""") {
+            "And: a valid value containing 'value'" {
                 value = "value"
             }
-            And("""a System Property defined""") {
+            "And: a System Property defined" {
                 System.setProperty(key, value)
             }
-            When("""a #getProperty(key) is requested""") {
+            "When: a #getProperty(key) is requested" {
                 result = PropHandler.getProperty(key)
             }
-            Then("""the property value is retrieved as expected""") {
+            "Then: the property value is retrieved as expected" {
                 assertThat(result).isEqualTo(value)
             }
         }
 
-        Scenario("""invalid key given to #getProperty""") {
+        "Scenario: invalid key given to #getProperty" - {
             val message = "Property 'invalidkey' is missing!"
             lateinit var key: String
             lateinit var assertion: AbstractThrowableAssert<*, out Throwable>
 
-            Given("""an invalid key""") {
+            "Given: an invalid key" {
                 key = "invalidkey"
             }
-            When("""a #getProperty(key) is requested""") {
+            "When: a #getProperty(key) is requested" {
                 assertion = Assertions.assertThatCode {
                     PropHandler.getProperty<String>(key)
                 }
             }
-            Then("""#getProperty(key) throws exception""") {
+            "Then: #getProperty(key) throws exception" {
                 assertion.hasSameClassAs(IllegalArgumentException(message))
             }
-            And("""exception has message '$message'""") {
+            "And: exception has message '$message'" {
                 assertion.hasMessage(message)
             }
         }
 
-        Scenario("""valid key given to #getProperty, no defaultValue usage""") {
+        "Scenario: valid key given to #getProperty, no defaultValue usage" - {
             lateinit var key: String
             var value = 0
             var defaultValue = 0
             var result = 0
 
-            Given("""a valid key named 'key'""") {
+            "Given: a valid key named 'key'" {
                 key = "key"
             }
-            And("""a valid value containing 'value'""") {
+            "And: a valid value containing 'value'" {
                 value = 100
             }
-            And("""a valid default value containing 'defaultValue'""") {
+            "And: a valid default value containing 'defaultValue'" {
                 defaultValue = 200
             }
-            And("""a System Property defined""") {
+            "And: a System Property defined" {
                 System.setProperty(key, value.toString())
             }
-            When("""a #getProperty(key, defaultValue) is requested""") {
+            "When: a #getProperty(key, defaultValue) is requested" {
                 result = PropHandler.getProperty(key, defaultValue)
             }
-            Then("""the property value is retrieved as expected""") {
+            "Then: the property value is retrieved as expected" {
                 assertThat(result).isEqualTo(value)
             }
-            And("""the property value is not defaultValue""") {
+            "And: the property value is not defaultValue" {
                 assertThat(result).isNotEqualTo(defaultValue)
             }
         }
 
-        Scenario("""invalid key given to #getProperty, defaultValue usage""") {
+        "Scenario: invalid key given to #getProperty, defaultValue usage" - {
             lateinit var key: String
             var defaultValue = 0
             var result = 0
 
-            Given("""an invalid key named 'invalidkey'""") {
+            "Given: an invalid key named 'invalidkey'" {
                 key = "invalidkey"
             }
-            And("""a valid default value containing '10'""") {
+            "And: a valid default value containing '10'" {
                 defaultValue = 10
             }
-            When("""a #getProperty(key, defaultValue) is requested""") {
+            "When: a #getProperty(key, defaultValue) is requested" {
                 result = PropHandler.getProperty(key, defaultValue)
             }
-            Then("""the property value is not retrieved as expected""") {
+            "Then: the property value is not retrieved as expected" {
                 assertThat(result).isEqualTo(defaultValue)
             }
         }
