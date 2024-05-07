5893b36a6d2d94fd96ea141872a76f292016b77b
ACMattos <acmattos@gmail.com>
2024-05-07 13:46:32 -0300

    adding kotest

--- src/test/kotlin/br/com/acmattos/hdc/procedure/application/ProcedureQueryHandlerServiceTest.kt
+++ src/test/kotlin/br/com/acmattos/hdc/procedure/application/ProcedureQueryHandlerServiceTest.kt
@@ -15,131 +15,126 @@ import br.com.acmattos.hdc.procedure.port.persistence.mongodb.DocumentIndexedFie
 import br.com.acmattos.hdc.procedure.port.rest.FindTheProcedureRequest
 import br.com.acmattos.hdc.procedure.port.rest.ProcedureRequestBuilder
 import io.javalin.http.Context
-import io.mockk.Runs
-import io.mockk.every
-import io.mockk.just
-import io.mockk.mockk
-import io.mockk.verify
-import java.util.Optional
+import io.kotest.core.spec.style.FreeSpec
+import io.mockk.*
 import org.assertj.core.api.Assertions.assertThat
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.gherkin.Feature
+import java.util.*
 
 /**
  * @author ACMattos
  * @since 29/03/2022.
  */
-object ProcedureQueryHandlerServiceTest: Spek({
-    Feature("${ProcedureQueryHandlerService::class.java} usage") {
-        Scenario("handling ${FindAllProceduresQuery::class.java} successfully - results found") {
+class ProcedureQueryHandlerServiceTest: FreeSpec({
+    "Feature: ${ProcedureQueryHandlerService::class.java} usage" - {
+        "Scenario: handling FindAllProceduresQuery successfully - results found" - {
             lateinit var query: FindAllProceduresQuery
             lateinit var store: QueryStore<ProcedureQuery>
             lateinit var repository: Repository<Procedure>
             lateinit var service: ProcedureQueryHandlerService
             lateinit var result: QueryResult<Procedure>
-            Given("""a ${QueryStore::class.java} mock""") {
+            "Given: a ${QueryStore::class.java} mock" {
                 store = mockk()
             }
-            And("""QueryStore#addQuery just runs""") {
+            "And: QueryStore#addQuery just runs" {
                 every { store.addQuery(any<FindAllProceduresQuery>()) } just Runs
             }
-            And("""a ${Repository::class.java} mock""") {
+            "And: a ${Repository::class.java} mock" {
                 repository = mockk()
             }
-            And("""repository#findAllByPage returns a${PageResult::class.java.simpleName}""") {
+            "And: repository#findAllByPage returns a${PageResult::class.java.simpleName}" {
                 every {
                     repository.findAllByPage(any())
                 } returns PageResult.create(listOf(ProcedureBuilder.buildCreate()), Page.create(), 1)
             }
-            And("""a ${FindAllProceduresQuery::class.java} created from ${Context::class.java}""") {
+            "And: a FindAllProceduresQuery created from ${Context::class.java}" {
                 query = ProcedureRequestBuilder.buildFindAllProceduresRequest()
                     .toType() as FindAllProceduresQuery
             }
-            And("""a ${ProcedureQueryHandlerService::class.java} successfully instantiated""") {
+            "And: a ${ProcedureQueryHandlerService::class.java} successfully instantiated" {
                 service = ProcedureQueryHandlerService(store, repository)
             }
-            When("""#handle is executed""") {
+            "When: #handle is executed" {
                 result = service.handle(query)
             }
-            Then("""${QueryResult::class.java} is not empty""") {
+            "Then: ${QueryResult::class.java} is not empty" {
                 assertThat(result.results).isNotEmpty()
             }
-            And("""${QueryResult::class.java} size is one""") {
+            "And: ${QueryResult::class.java} size is one" {
                 assertThat(result.results.size).isEqualTo(1)
             }
-            And("""the repository is accessed""") {
+            "And: the repository is accessed" {
                 verify {
                     repository.findAllByPage(any())
                 }
             }
-            And("""the query store is accessed as well""") {
+            "And: the query store is accessed as well" {
                 verify {
                     store.addQuery(any<FindAllProceduresQuery>())
                 }
             }
         }
 
-        Scenario("handling ${FindAllProceduresQuery::class.java} successfully - results not found") {
+        "Scenario: handling FindAllProceduresQuery successfully - results not found" - {
             lateinit var query: FindAllProceduresQuery
             lateinit var store: QueryStore<ProcedureQuery>
             lateinit var repository: Repository<Procedure>
             lateinit var service: ProcedureQueryHandlerService
             lateinit var result: QueryResult<Procedure>
-            Given("""a ${QueryStore::class.java} mock""") {
+            "Given: a ${QueryStore::class.java} mock" {
                 store = mockk()
             }
-            And("""QueryStore#addQuery just runs""") {
+            "And: QueryStore#addQuery just runs" {
                 every { store.addQuery(any<FindAllProceduresQuery>()) } just Runs
             }
-            And("""a ${Repository::class.java} mock""") {
+            "And: a ${Repository::class.java} mock" {
                 repository = mockk()
             }
-            And("""repository#findAll returns null""") {
+            "And: repository#findAll returns null" {
                 every {
                     repository.findAllByPage(any())
                 } returns PageResult.create(listOf(), Page.create(), 0)
             }
-            And("""a ${FindAllProceduresQuery::class.java} created from ${Context::class.java}""") {
+            "And: a FindAllProceduresQuery created from ${Context::class.java}" {
                 query = ProcedureRequestBuilder.buildFindAllProceduresRequest()
                     .toType() as FindAllProceduresQuery
             }
-            And("""a ${ProcedureQueryHandlerService::class.java} successfully instantiated""") {
+            "And: a ${ProcedureQueryHandlerService::class.java} successfully instantiated" {
                 service = ProcedureQueryHandlerService(store, repository)
             }
-            When("""#handle is executed""") {
+            "When: #handle is executed" {
                 result = service.handle(query)
             }
-            Then("""${QueryResult::class.java} is empty""") {
+            "Then: ${QueryResult::class.java} is empty" {
                 assertThat(result.results).isEmpty()
             }
-            And("""the repository is accessed""") {
+            "And: the repository is accessed" {
                 verify {
                     repository.findAllByPage(any())
                 }
             }
-            And("""the query store is accessed as well""") {
+            "And: the query store is accessed as well" {
                 verify {
                     store.addQuery(any<FindAllProceduresQuery>())
                 }
             }
         }
 
-        Scenario("handling ${FindAllProceduresQuery::class.java} successfully - results found") {
+        "Scenario: handling FindAllProceduresQuery successfully - results found" - {
             lateinit var query: FindTheProcedureQuery
             lateinit var store: QueryStore<ProcedureQuery>
             lateinit var repository: Repository<Procedure>
             lateinit var service: ProcedureQueryHandlerService
             lateinit var result: QueryResult<Procedure>
-            Given("""a ${QueryStore::class.java} mock""") {
+            "Given: a ${QueryStore::class.java} mock" {
                 store = mockk()
             }
-            And("""QueryStore#addQuery just runs""") {
+            "And: QueryStore#addQuery just runs" {
                 every { store.addQuery(any<FindTheProcedureQuery>()) } just Runs
             }
-            And("""a ${Repository::class.java} mock""") {
+            "And: a ${Repository::class.java} mock" {
                 repository = mockk()
             }
-            And("""repository#findOneByFilter returns a dentist""") {
+            "And: repository#findOneByFilter returns a dentist" {
                 every {
                     repository.findOneByFilter(
                         EqFilter<String, String>(
@@ -149,23 +144,23 @@ object ProcedureQueryHandlerServiceTest: Spek({
                     )
                 } returns Optional.of(ProcedureBuilder.buildCreate())
             }
-            And("""a ${FindTheProcedureRequest::class.java} created from ${Context::class.java}""") {
+            "And: a ${FindTheProcedureRequest::class.java} created from ${Context::class.java}" {
                 query = ProcedureRequestBuilder.buildFindTheProcedureRequest()
                     .toType() as FindTheProcedureQuery
             }
-            And("""a ${ProcedureQueryHandlerService::class.java} successfully instantiated""") {
+            "And: a ${ProcedureQueryHandlerService::class.java} successfully instantiated" {
                 service = ProcedureQueryHandlerService(store, repository)
             }
-            When("""#handle is executed""") {
+            "When: #handle is executed" {
                 result = service.handle(query)
             }
-            Then("""${QueryResult::class.java} is not empty""") {
+            "Then: ${QueryResult::class.java} is not empty" {
                 assertThat(result.results).isNotEmpty()
             }
-            And("""${QueryResult::class.java} size is one""") {
+            "And: ${QueryResult::class.java} size is one" {
                 assertThat(result.results.size).isEqualTo(1)
             }
-            And("""the repository is accessed""") {
+            "And: the repository is accessed" {
                 verify {
                     repository.findOneByFilter(
                         EqFilter<String, String>(
@@ -175,7 +170,7 @@ object ProcedureQueryHandlerServiceTest: Spek({
                     )
                 }
             }
-            And("""the query store is accessed as well""") {
+            "And: the query store is accessed as well" {
                 verify {
                     store.addQuery(any<FindTheProcedureQuery>())
                 }
