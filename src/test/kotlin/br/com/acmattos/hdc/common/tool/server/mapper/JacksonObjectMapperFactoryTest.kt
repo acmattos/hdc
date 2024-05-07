d0c2ad65af44f09250e62c173d36078a3b04ca4a
ACMattos <acmattos@gmail.com>
2024-05-07 12:39:16 -0300

    adding kotest

--- src/test/kotlin/br/com/acmattos/hdc/common/tool/server/mapper/JacksonObjectMapperFactoryTest.kt
+++ src/test/kotlin/br/com/acmattos/hdc/common/tool/server/mapper/JacksonObjectMapperFactoryTest.kt
@@ -1,26 +1,25 @@
 package br.com.acmattos.hdc.common.tool.server.mapper
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.kotest.core.spec.style.FreeSpec
 import org.assertj.core.api.Assertions.assertThat
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.gherkin.Feature
 
 /**
  * @author ACMattos
  * @since 26/09/2021.
  */
-object JacksonObjectMapperFactoryTest: Spek({
-    Feature("${JacksonObjectMapperFactory::class.java} usage") {
-        Scenario("plugin configuration is done successfully") {
+class JacksonObjectMapperFactoryTest: FreeSpec({
+    "Feature: ${JacksonObjectMapperFactory::class.java} usage" - {
+        "Scenario: plugin configuration is done successfully" - {
             lateinit var factory: JacksonObjectMapperFactory
             lateinit var mapper: ObjectMapper
-            Given("""a ${JacksonObjectMapperFactory::class.java}""") {
+            "Given: a ${JacksonObjectMapperFactory::class.java}" {
                 factory = JacksonObjectMapperFactory
             }
-            When("""#build is executed""") {
+            "When: #build is executed" {
                 mapper =  factory.build()
             }
-            Then("""a mapper is returned""") {
+            "Then: a mapper is returned" {
                 assertThat(mapper).isNotNull()
             }
         }
