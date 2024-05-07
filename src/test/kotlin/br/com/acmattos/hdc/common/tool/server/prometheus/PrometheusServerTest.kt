bed726bb92886a1a8f789fab13bc023ad78d4e0d
ACMattos <acmattos@gmail.com>
2024-05-07 12:53:10 -0300

    adding kotest

--- src/test/kotlin/br/com/acmattos/hdc/common/tool/server/prometheus/PrometheusServerTest.kt
+++ src/test/kotlin/br/com/acmattos/hdc/common/tool/server/prometheus/PrometheusServerTest.kt
@@ -3,76 +3,75 @@ package br.com.acmattos.hdc.common.tool.server.prometheus
 import br.com.acmattos.hdc.common.tool.HttpClient
 import com.github.kittinunf.fuel.core.Response
 import io.javalin.plugin.openapi.annotations.ContentType
+import io.kotest.core.spec.style.FreeSpec
 import org.assertj.core.api.Assertions.assertThat
 import org.eclipse.jetty.server.handler.StatisticsHandler
 import org.eclipse.jetty.util.thread.QueuedThreadPool
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.gherkin.Feature
 
 /**
  * @author ACMattos
  * @since 09/06/2020.
  */
-object PrometheusServerTest : Spek({
-    Feature("${PrometheusServer::class.java} usage") {
+class PrometheusServerTest : FreeSpec({
+    "Feature: ${PrometheusServer::class.java} usage" - {
         val port = 7777
         lateinit var statisticsHandler: StatisticsHandler
         lateinit var queuedThreadPool: QueuedThreadPool
         lateinit var prometheusServer: PrometheusServer
 
-        afterEachScenario { prometheusServer.stop() }
-
-        Scenario("${PrometheusServer::class.java} successfully started usage") {
+        "Scenario: ${PrometheusServer::class.java} successfully started usage" - {
             lateinit var response: Response
-            Given("""a statistics handler""") {
+            "Given: a statistics handler" {
                 statisticsHandler = StatisticsHandler()
             }
-            And("""a queued thread pool""") {
+            "And: a queued thread pool" {
                 queuedThreadPool = QueuedThreadPool(300,50,60000)
             }
-            And("""a prometheus server instantiated and started""") {
+            "And: a prometheus server instantiated and started" {
                 prometheusServer = PrometheusServer(
                     statisticsHandler = statisticsHandler,
                     queuedThreadPool = queuedThreadPool
                 ).start(port)
             }
-            When("""a HTTP GET connection can be done to prometheus""") {
+            "When: a HTTP GET connection can be done to prometheus" {
                 response = HttpClient.port(port).get("")
                 StatisticsHandlerCollector.finalize()
                 QueuedThreadPoolCollector.finalize()
             }
-            Then("""response status is 200 OK""") {
+            "Then: response status is 200 OK" {
                 assertThat(response.statusCode).isEqualTo(200)
             }
-            And("""the response body contains # TYPE jetty_queued_thread_pool_threads gauge"""){
+            "And: the response body contains # TYPE jetty_queued_thread_pool_threads gauge"{
                 assertThat(response.body().asString(ContentType.JSON))
                     .contains("# TYPE jetty_queued_thread_pool_threads gauge")
             }
+            prometheusServer.stop()
         }
 
-        Scenario("${PrometheusServer::class.java} successfully stopped usage") {
+        "Scenario: ${PrometheusServer::class.java} successfully stopped usage" - {
             lateinit var response: Response
-            Given("""a statistics handler""") {
+            "Given: a statistics handler" {
                 statisticsHandler = StatisticsHandler()
             }
-            And("""a queued thread pool""") {
+            "And: a queued thread pool" {
                 queuedThreadPool = QueuedThreadPool(300,50,60000)
             }
-            And("""a prometheus server instantiated" and stopped""") {
+            "And: a prometheus server instantiated and stopped" {
                 prometheusServer = PrometheusServer(
                     statisticsHandler = statisticsHandler,
                     queuedThreadPool = queuedThreadPool
                 ).start(port).stop()
             }
-            When("""an HTTP connection can be done to prometheus""") {
+            "When: an HTTP connection can be done to prometheus" {
                 response = HttpClient.port(port).get("")
             }
-            Then("""response status is -1 (Fuel Status)""") {
+            "Then: response status is -1 (Fuel Status)" {
                 assertThat(response.statusCode).isEqualTo(-1)
             }
-            And(""" the response body is empty"""){
+            "And:  the response body is empty"{
                 assertThat(response.body().isEmpty()).isTrue()
             }
+            prometheusServer.stop()
         }
     }
 })
--- src/test/kotlin/br/com/acmattos/hdc/common/tool/server/prometheus/StatisticsHandlerCollectorTest.kt
+++ src/test/kotlin/br/com/acmattos/hdc/common/tool/server/prometheus/StatisticsHandlerCollectorTest.kt
@@ -1,43 +1,42 @@
 package br.com.acmattos.hdc.common.tool.server.prometheus
 
+import io.kotest.core.spec.style.FreeSpec
 import io.mockk.mockk
 import io.prometheus.client.Collector
 import org.assertj.core.api.Assertions.assertThat
 import org.eclipse.jetty.server.handler.StatisticsHandler
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.gherkin.Feature
 
 /**
  * @author ACMattos
  * @since 09/06/2020.
  */
-object StatisticsHandlerCollectorTest : Spek({
-    Feature("${StatisticsHandlerCollector::class.java} usage") {
-        beforeEachScenario { StatisticsHandlerCollector.finalize() }
-        Scenario("statistics handler collector successfully initialized") {
+class StatisticsHandlerCollectorTest : FreeSpec({
+    "Feature: ${StatisticsHandlerCollector::class.java} usage" - {
+        "Scenario: statistics handler collector successfully initialized" - {
             lateinit var handler: StatisticsHandler
             lateinit var collector: StatisticsHandlerCollector
             lateinit var samples: List<Collector.MetricFamilySamples>
-            Given("""a statistics handler""") {
+            "Given: a statistics handler" {
                 handler = mockk(relaxed = true)
             }
-            And("""a statistics handler collector for a set of metrics""") {
+            "And: a statistics handler collector for a set of metrics" {
                 collector = StatisticsHandlerCollector.initialize(
                     statisticsHandler = handler,
                     enableDefaultRegistry = false
                 )
             }
-            When("""an collect operation is done""") {
+            "When: an collect operation is done" {
                 samples = collector.collect()
             }
-            Then(""" the list of ${Collector.MetricFamilySamples::class.java} has size 18"""){
+            "Then:  the list of ${Collector.MetricFamilySamples::class.java} has size 18"{
                 assertThat(samples).hasSize(18)
             }
-            And(""" the type GAUGE has size 9"""){
+            "And:  the type GAUGE has size 9"{
                 val samplesByType = samples.groupBy { it.type }
                 assertThat(samplesByType[Collector.Type.GAUGE]).hasSize(9)
                 StatisticsHandlerCollector.finalize()
             }
         }
+        StatisticsHandlerCollector.finalize()
     }
 })
