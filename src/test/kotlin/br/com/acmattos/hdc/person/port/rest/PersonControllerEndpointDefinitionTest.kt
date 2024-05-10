2db818d48c1b11013dcef96674dd2aedac9dd413
ACMattos <acmattos@gmail.com>
2024-05-10 13:30:47 -0300

    adding kotest

--- src/test/kotlin/br/com/acmattos/hdc/person/port/rest/PersonControllerEndpointDefinitionTest.kt
+++ src/test/kotlin/br/com/acmattos/hdc/person/port/rest/PersonControllerEndpointDefinitionTest.kt
@@ -3,33 +3,32 @@ package br.com.acmattos.hdc.person.port.rest
 import br.com.acmattos.hdc.common.context.port.rest.EndpointDefinition
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder
+import io.kotest.core.spec.style.FreeSpec
 import io.mockk.mockk
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.gherkin.Feature
 
 /**
  * @author ACMattos
  * @since 05/10/2021.
  */
-object PersonControllerEndpointDefinitionTest: Spek({
-    Feature("${PersonControllerEndpointDefinition::class.java} existence") {
-        Scenario("endpoint definition exists") {
+class PersonControllerEndpointDefinitionTest: FreeSpec({
+    "Feature: PersonControllerEndpointDefinition existence" - {
+        "Scenario: endpoint definition exists" - {
             lateinit var ccontroller: PersonCommandController
             lateinit var qcontroller: PersonQueryController
             lateinit var endpoint: EndpointDefinition
-            Given("""a mocked command controller""") {
+            "Given: a mocked command controller" {
                 ccontroller = mockk()
             }
-            And("""a mocked query controller""") {
+            "And: a mocked query controller" {
                 qcontroller = mockk()
             }
-            And("""a javalin server is configured""") {
+            "And: a javalin server is configured" {
                 ApiBuilder.setStaticJavalin(Javalin.create())
             }
-            When("""a ${PersonControllerEndpointDefinition::class.java} is successfully instantiated""") {
+            "When: a ${PersonControllerEndpointDefinition::class.java} is successfully instantiated" {
                 endpoint = PersonControllerEndpointDefinition(ccontroller, qcontroller)
             }
-            Then("""#routes can be called""") {
+            "Then: #routes can be called" {
                 endpoint.routes()
             }
         }
